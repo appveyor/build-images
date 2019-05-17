040954e70c7fa6d868b0d0f4d436c802daf7055a
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-05-17 10:49:58 -0700

    v1.16.0

--- scripts/Windows/install_yarn.ps1
+++ scripts/Windows/install_yarn.ps1
@@ -1,8 +1,8 @@
-﻿Write-Host "Installing Yarn..." -ForegroundColor Cyan
+Write-Host "Installing Yarn..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\yarn.msi"
-(New-Object Net.WebClient).DownloadFile('https://github.com/yarnpkg/yarn/releases/download/v1.9.4/yarn-1.9.4.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/yarnpkg/yarn/releases/download/v1.16.0/yarn-1.16.0.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /quiet
