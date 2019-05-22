ebb804cd404d77a3c74b6b2d513943f81e71f09f
Mark Lopez <m@silvenga.com>
2019-05-22 15:50:19 -0400

    Fixed install_wix311.ps1 not waiting for wix to completely install before continuing.

--- scripts/Windows/install_wix311.ps1
+++ scripts/Windows/install_wix311.ps1
@@ -5,7 +5,7 @@ $exePath = "$env:TEMP\wix311.exe"
 (New-Object Net.WebClient).DownloadFile('http://wixtoolset.org/downloads/v3.11.0.1528/wix311.exe', $exePath)
 
 Write-Host "Installing..."
-cmd /c start "$exePath" /q
-del $exePath
+cmd /c start /wait "$exePath" /q
+Remove-Item $exePath
 
 Write-Host "WiX 3.11 installed" -ForegroundColor Green
