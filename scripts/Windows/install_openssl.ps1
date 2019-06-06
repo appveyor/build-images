3ea7556ad0f4bda26a8fb65ce2384503ac6862ec
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-06-06 00:29:40 -0700

    OpenSSl 1_1_0k, 1_1_1c, 1_0_2s
    https://github.com/appveyor/ci/issues/2967

--- scripts/Windows/install_openssl.ps1
+++ scripts/Windows/install_openssl.ps1
@@ -37,8 +37,8 @@ Remove-Item C:\OpenSSL-v111-Win64 -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Installing OpenSSL 1.1.0 32-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win32OpenSSL-1_1_0j.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_1_0j.exe', $exePath)
+$exePath = "$env:temp\Win32OpenSSL-1_1_0k.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_1_0k.exe', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v11-Win32-temp
 Write-Host "Installed" -ForegroundColor Green
@@ -49,8 +49,8 @@ Copy-Item C:\OpenSSL-v11-Win32-temp -Destination C:\OpenSSL-v11-Win32 -Recurse
 
 Write-Host "Installing OpenSSL 1.1.0 64-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win64OpenSSL-1_1_0j.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_1_0j.exe', $exePath)
+$exePath = "$env:temp\Win64OpenSSL-1_1_0k.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_1_0k.exe', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v11-Win64-temp
 Write-Host "Installed" -ForegroundColor Green
@@ -65,8 +65,8 @@ UninstallOpenSSL "OpenSSL 1.1.0j (64-bit)"
 
 Write-Host "Installing OpenSSL 1.1.1 32-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win32OpenSSL-1_1_1b.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_1_1b.exe', $exePath)
+$exePath = "$env:temp\Win32OpenSSL-1_1_1c.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_1_1c.exe', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v111-Win32-temp
 Write-Host "Installed" -ForegroundColor Green
@@ -77,8 +77,8 @@ Copy-Item C:\OpenSSL-v111-Win32-temp -Destination C:\OpenSSL-v111-Win32 -Recurse
 
 Write-Host "Installing OpenSSL 1.1.1 64-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win64OpenSSL-1_1_1b.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_1_1b.exe', $exePath)
+$exePath = "$env:temp\Win64OpenSSL-1_1_1c.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_1_1c.exe', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v111-Win64-temp
 Write-Host "Installed" -ForegroundColor Green
@@ -93,8 +93,8 @@ UninstallOpenSSL "OpenSSL 1.1.1b (64-bit)"
 Write-Host "Installing OpenSSL 1.0.x 32-bit ..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win32OpenSSL-1_0_2r.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_0_2r.exe', $exePath)
+$exePath = "$env:temp\Win32OpenSSL-1_0_2s.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win32OpenSSL-1_0_2s.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes
@@ -108,8 +108,8 @@ Write-Host "Installed" -ForegroundColor Green
 Write-Host "Installing OpenSSL 1.0.x 64-bit ..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win64OpenSSL-1_0_2r.exe"
-(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_0_2r.exe', $exePath)
+$exePath = "$env:temp\Win64OpenSSL-1_0_2s.exe"
+(New-Object Net.WebClient).DownloadFile('https://slproweb.com/download/Win64OpenSSL-1_0_2s.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes
