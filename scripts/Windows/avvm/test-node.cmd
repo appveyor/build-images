03abb53c1a24f418481a0d17ab19296d7dab3487
Feodor Fitsner <feodor@appveyor.com>
2019-06-11 13:14:35 -0700

    Node.js 12.x added

--- scripts/Windows/avvm/test-node.cmd
+++ scripts/Windows/avvm/test-node.cmd
@@ -1,30 +1,22 @@
-c:\avvm\node\6.17.0\x86\nodejs\node --version
-c:\avvm\node\6.17.0\x64\nodejs\node --version
+c:\avvm\node\8.16.0\x86\nodejs\node --version
+c:\avvm\node\8.16.0\x64\nodejs\node --version
 
-c:\avvm\node\6.17.1\x86\nodejs\node --version
-c:\avvm\node\6.17.1\x64\nodejs\node --version
+c:\avvm\node\10.16.0\x86\nodejs\node --version
+c:\avvm\node\10.16.0\x64\nodejs\node --version
 
-c:\avvm\node\8.15.1\x86\nodejs\node --version
-c:\avvm\node\8.15.1\x64\nodejs\node --version
+c:\avvm\node\11.14.0\x86\nodejs\node --version
+c:\avvm\node\11.14.0\x64\nodejs\node --version
 
-c:\avvm\node\10.15.2\x86\nodejs\node --version
-c:\avvm\node\10.15.2\x64\nodejs\node --version
+c:\avvm\node\11.15.0\x86\nodejs\node --version
+c:\avvm\node\11.15.0\x64\nodejs\node --version
 
-c:\avvm\node\10.15.3\x86\nodejs\node --version
-c:\avvm\node\10.15.3\x64\nodejs\node --version
+c:\avvm\node\12.0.0\x86\nodejs\node --version
+c:\avvm\node\12.0.0\x64\nodejs\node --version
 
-c:\avvm\node\11.10.0\x86\nodejs\node --version
-c:\avvm\node\11.10.0\x64\nodejs\node --version
+c:\avvm\node\12.1.0\x86\nodejs\node --version
+c:\avvm\node\12.1.0\x64\nodejs\node --version
 
-c:\avvm\node\11.10.1\x86\nodejs\node --version
-c:\avvm\node\11.10.1\x64\nodejs\node --version
+c:\avvm\node\12.4.0\x86\nodejs\node --version
+c:\avvm\node\12.4.0\x64\nodejs\node --version
 
-c:\avvm\node\11.11.0\x86\nodejs\node --version
-c:\avvm\node\11.11.0\x64\nodejs\node --version
-
-c:\avvm\node\11.12.0\x86\nodejs\node --version
-c:\avvm\node\11.12.0\x64\nodejs\node --version
-
-c:\avvm\node\11.13.0\x86\nodejs\node --version
-c:\avvm\node\11.13.0\x64\nodejs\node --version
 pause
