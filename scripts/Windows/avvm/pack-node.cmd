af1670fe3ec1475ecd6ea0f5d73430c47dde9030
Feodor Fitsner <feodor@appveyor.com>
2019-07-16 15:22:58 -0700

    Node.js 12.6.0

--- scripts/Windows/avvm/pack-node.cmd
+++ scripts/Windows/avvm/pack-node.cmd
@@ -1,20 +1,5 @@
-7z a c:\avvm\node\node-8.16.0-x86.7z c:\avvm\node\8.16.0\x86\*
-7z a c:\avvm\node\node-8.16.0-x64.7z c:\avvm\node\8.16.0\x64\*
+7z a c:\avvm\node\node-12.5.0-x86.7z c:\avvm\node\12.5.0\x86\*
+7z a c:\avvm\node\node-12.5.0-x64.7z c:\avvm\node\12.5.0\x64\*
 
-7z a c:\avvm\node\node-10.16.0-x86.7z c:\avvm\node\10.16.0\x86\*
-7z a c:\avvm\node\node-10.16.0-x64.7z c:\avvm\node\10.16.0\x64\*
-
-7z a c:\avvm\node\node-11.14.0-x86.7z c:\avvm\node\11.14.0\x86\*
-7z a c:\avvm\node\node-11.14.0-x64.7z c:\avvm\node\11.14.0\x64\*
-
-7z a c:\avvm\node\node-11.15.0-x86.7z c:\avvm\node\11.15.0\x86\*
-7z a c:\avvm\node\node-11.15.0-x64.7z c:\avvm\node\11.15.0\x64\*
-
-7z a c:\avvm\node\node-12.0.0-x86.7z c:\avvm\node\12.0.0\x86\*
-7z a c:\avvm\node\node-12.0.0-x64.7z c:\avvm\node\12.0.0\x64\*
-
-7z a c:\avvm\node\node-12.1.0-x86.7z c:\avvm\node\12.1.0\x86\*
-7z a c:\avvm\node\node-12.1.0-x64.7z c:\avvm\node\12.1.0\x64\*
-
-7z a c:\avvm\node\node-12.4.0-x86.7z c:\avvm\node\12.4.0\x86\*
-7z a c:\avvm\node\node-12.4.0-x64.7z c:\avvm\node\12.4.0\x64\*
+7z a c:\avvm\node\node-12.6.0-x86.7z c:\avvm\node\12.6.0\x86\*
+7z a c:\avvm\node\node-12.6.0-x64.7z c:\avvm\node\12.6.0\x64\*
