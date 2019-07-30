98b4edd9f76d8fb69533fe760996890262f2d66c
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-07-29 17:12:22 -0700

    Cleaner output

--- scripts/Windows/install_wdk_1903.ps1
+++ scripts/Windows/install_wdk_1903.ps1
@@ -5,7 +5,8 @@ $exePath = "$env:temp\wdksetup.exe"
 (New-Object Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=2085767', $exePath)
 Write-Host "Installing..."
 cmd /c start /wait $exePath /quiet
-Remove-Item $exePath
+Remove-Item $exePath -Force -ErrorAction Ignore
+Write-Host "OK"
 
 if (-not (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community")) {
   return
@@ -16,4 +17,5 @@ $vsVersion=& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.e
 $vsVersion = $vsVersion -replace "`n|`r"
 "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\VSIXInstaller.exe`"  /a /f /sp /q /skuName:Community /skuVersion:$vsVersion `"C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix`"" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
-Remove-Item .\install-vsix.cmd
+Remove-Item .\install-vsix.cmd -Force -ErrorAction Ignore
+Write-Host "OK"
