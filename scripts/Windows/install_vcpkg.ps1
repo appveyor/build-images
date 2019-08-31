d4a5e915fb737040cfb56d5d0e9e795b0af920ee
craftwar <16416710+craftwar@users.noreply.github.com>
2019-08-31 13:56:16 +0800

    Hook up user-wide integration
    Make vcpkg available to Visual Studio without additional setup

--- scripts/Windows/install_vcpkg.ps1
+++ scripts/Windows/install_vcpkg.ps1
@@ -3,11 +3,13 @@ if (test-path C:\Tools\vcpkg) {
   Push-Location C:\Tools\vcpkg
   cmd /c git pull
   .\bootstrap-vcpkg.bat
+  .\vcpkg integrate install
   }
 else {
   Push-Location C:\Tools
   git clone https://github.com/Microsoft/vcpkg
   .\vcpkg\bootstrap-vcpkg.bat
+  .\vcpkg integrate install
   Add-Path C:\Tools\vcpkg
   Add-SessionPath C:\Tools\vcpkg
 }
