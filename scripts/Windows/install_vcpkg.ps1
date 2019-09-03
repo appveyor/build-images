96b6710ccae211b2e1341d295872aab7609fcdb0
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-09-02 17:58:56 -0700

    vcpkg integrate install after added to the path

--- scripts/Windows/install_vcpkg.ps1
+++ scripts/Windows/install_vcpkg.ps1
@@ -8,10 +8,10 @@ if (test-path C:\Tools\vcpkg) {
 else {
   Push-Location C:\Tools
   git clone https://github.com/Microsoft/vcpkg
-  .\vcpkg\bootstrap-vcpkg.bat
-  .\vcpkg integrate install
+  .\vcpkg\bootstrap-vcpkg.bat  
   Add-Path C:\Tools\vcpkg
   Add-SessionPath C:\Tools\vcpkg
+  .\vcpkg integrate install
 }
 Pop-Location
 vcpkg version | findstr /psi "version"
