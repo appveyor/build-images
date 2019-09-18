35fdebde23a5b57ac2653b588310243ec7a31774
Feodor Fitsner <feodor@appveyor.com>
2019-09-18 10:54:49 -0700

    set execution rights

---
+++ scripts/Ubuntu/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+if [ -d /appveyor/temp ]; then
+    sudo chown -R $(id -u):$(id -g) /appveyor/temp
+fi
+
+if [ -d /appveyor/bin ]; then
+    sudo chown -R $(id -u):$(id -g) /appveyor/bin
+fi
+
+if [[ ! -z "${APPVEYOR_BUILD_FOLDER}" ]]; then
+    sudo chown -R $(id -u):$(id -g) ${APPVEYOR_BUILD_FOLDER}
+fi
+
+# execute build agent
+/opt/appveyor/build-agent/appveyor-build-agent
