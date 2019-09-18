0ea7995d870e194f630aeffda160c5eb9433bff2
Feodor Fitsner <feodor@appveyor.com>
2019-09-18 11:02:22 -0700

    Do not fail on inbound variable

--- scripts/Ubuntu/entrypoint.sh
+++ scripts/Ubuntu/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -d /appveyor/bin ]; then
     sudo chown -R $(id -u):$(id -g) /appveyor/bin
 fi
 
-if [[ ! -z "${APPVEYOR_BUILD_FOLDER}" ]]; then
+if [[ ! -z "${APPVEYOR_BUILD_FOLDER-}" ]]; then
     sudo chown -R $(id -u):$(id -g) ${APPVEYOR_BUILD_FOLDER}
 fi
 
