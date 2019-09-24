f72dfd9bb2855a89fe311b3681cddcd3b3b7153c
Vasily Pleshakov <wasa@yandex.ru>
2019-09-23 18:15:05 -0700

    Install .NET Core 3.0 release in Ubuntus

--- scripts/Ubuntu/bionic.sh
+++ scripts/Ubuntu/bionic.sh
@@ -59,11 +59,14 @@ function install_pip() {
 }
 
 function prepare_dotnet_packages() {
-    declare SDK_VERSIONS=( "2.1.105" "2.1.200" "2.1.201" "2.1" "2.2" )
+    declare SDK_VERSIONS=( "2.1.105" "2.1.200" "2.1.201" "2.1" "2.2" "3.0" )
     dotnet_packages "dotnet-sdk-" SDK_VERSIONS[@]
 
     declare RUNTIME_VERSIONS=( "2.0.7" "2.1" "2.2" )
     dotnet_packages "dotnet-runtime-" RUNTIME_VERSIONS[@]
+
+    declare RUNTIME_VERSIONS=( "2.1" "2.2" "3.0" )
+    dotnet_packages "aspnetcore-runtime-" RUNTIME_VERSIONS[@]
 
     declare DEV_VERSIONS=( "1.1.5" "1.1.6" "1.1.7" "1.1.8" "1.1.9" "1.1.11" )
     dotnet_packages "dotnet-dev-" DEV_VERSIONS[@]
