94ff50d585991c7aed86884632930c9ea6b510b4
Feodor Fitsner <feodor@appveyor.com>
2019-10-03 19:56:20 -0700

    Install Qt

--- scripts/Windows/install_qt.ps1
+++ scripts/Windows/install_qt.ps1
@@ -4,8 +4,13 @@ Write-Host "Downloading Qt Installer..."
 $exePath = "$env:TEMP\qt-unified-windows-x86-online.exe"
 (New-Object Net.WebClient).DownloadFile('http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe', $exePath)
 
+$qsPath = "$PSScriptRoot\qt-installer-windows.qs"
+if (-not (Test-Path $qsPath)) {
+    $qsPath = "$env:USERPROFILE\qt-installer-windows.qs"
+}
+
 Write-Host "Installing..."
-cmd /c "$exePath" --verbose --script "$PSScriptRoot\qt-installer-windows.qs"
+cmd /c "$exePath" --verbose --script "$qsPath"
 Remove-Item $exePath
 
 Write-Host "Qt 5.x installed" -ForegroundColor Green
