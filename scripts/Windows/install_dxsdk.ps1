62208d7352e003b99f1ff976ec856cd6d9215e1c
Feodor Fitsner <feodor@appveyor.com>
2019-10-08 06:53:23 -0700

    Update install_dxsdk.ps1

--- scripts/Windows/install_dxsdk.ps1
+++ scripts/Windows/install_dxsdk.ps1
@@ -1,5 +1,9 @@
 ﻿Write-Host "DirectX Software Development Kit..." -ForegroundColor Cyan
 
+$ErrorActionPreference = "Stop"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Write-Host "Downloading..."
 $exePath = "$env:temp\DXSDK_Jun10.exe"
 (New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe', $exePath)
