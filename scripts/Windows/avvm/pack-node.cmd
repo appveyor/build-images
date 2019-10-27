d4f019a710f4237b6544ce0d4f0635e22288fbb4
Feodor Fitsner <feodor@appveyor.com>
2019-10-27 14:28:16 -0700

    Node.js updated

--- scripts/Windows/avvm/pack-node.cmd
+++ scripts/Windows/avvm/pack-node.cmd
@@ -1,5 +1,17 @@
-7z a c:\avvm\node\node-12.5.0-x86.7z c:\avvm\node\12.5.0\x86\*
-7z a c:\avvm\node\node-12.5.0-x64.7z c:\avvm\node\12.5.0\x64\*
+7z a c:\avvm\node\node-10.17.0-x86.7z c:\avvm\node\10.17.0\x86\*
+7z a c:\avvm\node\node-10.17.0-x64.7z c:\avvm\node\10.17.0\x64\*
 
-7z a c:\avvm\node\node-12.6.0-x86.7z c:\avvm\node\12.6.0\x86\*
-7z a c:\avvm\node\node-12.6.0-x64.7z c:\avvm\node\12.6.0\x64\*
+7z a c:\avvm\node\node-12.12.0-x86.7z c:\avvm\node\12.12.0\x86\*
+7z a c:\avvm\node\node-12.12.0-x64.7z c:\avvm\node\12.12.0\x64\*
+
+7z a c:\avvm\node\node-12.13.0-x86.7z c:\avvm\node\12.13.0\x86\*
+7z a c:\avvm\node\node-12.13.0-x64.7z c:\avvm\node\12.13.0\x64\*
+
+7z a c:\avvm\node\node-13.0.0-x86.7z c:\avvm\node\13.0.0\x86\*
+7z a c:\avvm\node\node-13.0.0-x64.7z c:\avvm\node\13.0.0\x64\*
+
+7z a c:\avvm\node\node-13.0.1-x86.7z c:\avvm\node\13.0.1\x86\*
+7z a c:\avvm\node\node-13.0.1-x64.7z c:\avvm\node\13.0.1\x64\*
+
+7z a c:\avvm\node\node-8.16.2-x86.7z c:\avvm\node\8.16.2\x86\*
+7z a c:\avvm\node\node-8.16.2-x64.7z c:\avvm\node\8.16.2\x64\*
--- scripts/Windows/avvm/test-node.cmd
+++ scripts/Windows/avvm/test-node.cmd
@@ -1,7 +1,19 @@
-c:\avvm\node\12.5.0\x86\nodejs\node --version
-c:\avvm\node\12.5.0\x64\nodejs\node --version
+c:\avvm\node\10.17.0\x86\nodejs\node --version
+c:\avvm\node\10.17.0\x64\nodejs\node --version
 
-c:\avvm\node\12.6.0\x86\nodejs\node --version
-c:\avvm\node\12.6.0\x64\nodejs\node --version
+c:\avvm\node\12.12.0\x86\nodejs\node --version
+c:\avvm\node\12.12.0\x64\nodejs\node --version
+
+c:\avvm\node\12.13.0\x86\nodejs\node --version
+c:\avvm\node\12.13.0\x64\nodejs\node --version
+
+c:\avvm\node\13.0.0\x86\nodejs\node --version
+c:\avvm\node\13.0.0\x64\nodejs\node --version
+
+c:\avvm\node\13.0.1\x86\nodejs\node --version
+c:\avvm\node\13.0.1\x64\nodejs\node --version
+
+c:\avvm\node\8.16.2\x86\nodejs\node --version
+c:\avvm\node\8.16.2\x64\nodejs\node --version
 
 pause
