4379c21eb932a8bee51e6df53850d1179220f59b
Feodor Fitsner <feodor@appveyor.com>
2019-11-08 17:53:12 -0800

    Remove ENABLE_PSREMOTING=1

--- scripts/Windows/install_powershell_core.ps1
+++ scripts/Windows/install_powershell_core.ps1
@@ -8,7 +8,7 @@ $msiPath = "$env:TEMP\PowerShell-Core.msi"
 (New-Object Net.WebClient).DownloadFile('https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/PowerShell-6.2.3-win-x64.msi', $msiPath)
 
 Write-Host "Installing..."
-cmd /c start /wait msiexec /i $msiPath /quiet ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+cmd /c start /wait msiexec /i $msiPath /quiet REGISTER_MANIFEST=1
 del $msiPath
 Add-SessionPath "$env:ProgramFiles\PowerShell\6"
 
