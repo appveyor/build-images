7ddb72bb250cac95ae36be256fcd72aef1e26701
Feodor Fitsner <feodor@appveyor.com>
2019-11-13 10:05:53 -0800

    Add python 3.8 to cygwin

--- scripts/Windows/install_cygwin.ps1
+++ scripts/Windows/install_cygwin.ps1
@@ -11,7 +11,7 @@ $exePath = "C:\cygwin\setup-x86.exe"
 (New-Object Net.WebClient).DownloadFile('https://cygwin.com/setup-x86.exe', $exePath)
 
 # install cygwin
-cmd /c start /wait $exePath -qnNdO -R C:/cygwin -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P mingw64-i686-gcc-g++ -P mingw64-x86_64-gcc-g++ -P gcc-g++ -P autoconf -P automake -P bison -P libtool -P make -P python2 -P python -P gettext-devel -P intltool -P libiconv -P pkg-config -P wget -P curl
+cmd /c start /wait $exePath -qnNdO -R C:/cygwin -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P mingw64-i686-gcc-g++ -P mingw64-x86_64-gcc-g++ -P gcc-g++ -P autoconf -P automake -P bison -P libtool -P make -P python2 -P python -P python38 -P gettext-devel -P intltool -P libiconv -P pkg-config -P wget -P curl
 C:\Cygwin\bin\bash -lc true
 
 cmd /c "C:\cygwin\bin\cygcheck -c | C:\cygwin\bin\grep cygwin"
@@ -35,7 +35,7 @@ $exePath = "C:\cygwin64\setup-x86_64.exe"
 (New-Object Net.WebClient).DownloadFile('https://cygwin.com/setup-x86_64.exe', $exePath)
 
 # install cygwin
-cmd /c start /wait $exePath -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin64/var/cache/setup -P mingw64-i686-gcc-g++ -P mingw64-x86_64-gcc-g++ -P gcc-g++ -P autoconf -P automake -P bison -P libtool -P make -P python2 -P python -P gettext-devel -P intltool -P libiconv -P pkg-config -P wget -P curl
+cmd /c start /wait $exePath -qnNdO -R C:/cygwin64 -s http://cygwin.mirror.constant.com -l C:/cygwin64/var/cache/setup -P mingw64-i686-gcc-g++ -P mingw64-x86_64-gcc-g++ -P gcc-g++ -P autoconf -P automake -P bison -P libtool -P make -P python2 -P python -P python38 -P gettext-devel -P intltool -P libiconv -P pkg-config -P wget -P curl
 C:\cygwin64\bin\bash -lc true
 
 cmd /c "C:\cygwin64\bin\cygcheck -c | C:\cygwin64\bin\grep cygwin"
