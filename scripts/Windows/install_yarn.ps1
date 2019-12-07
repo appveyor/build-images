cd94fda0dbff67519ebab8a68f7565010ebb8b95
Feodor Fitsner <feodor@appveyor.com>
2019-12-06 20:05:35 -0800

    Install Yarn

--- scripts/Windows/install_yarn.ps1
+++ scripts/Windows/install_yarn.ps1
@@ -1,13 +1,5 @@
 Write-Host "Installing Yarn..." -ForegroundColor Cyan
 
-Write-Host "Downloading..."
-$msiPath = "$env:TEMP\yarn.msi"
-(New-Object Net.WebClient).DownloadFile('https://github.com/yarnpkg/yarn/releases/download/v1.16.0/yarn-1.16.0.msi', $msiPath)
-
-Write-Host "Installing..."
-cmd /c start /wait msiexec /i "$msiPath" /quiet
-del $msiPath
-
-Add-Path "${env:ProgramFiles(x86)}\Yarn\bin"
+choco install yarn --version=1.19.2
 
 Write-Host "Yarn installed" -ForegroundColor Green
