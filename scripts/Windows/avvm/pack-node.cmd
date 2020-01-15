9afb6eb98dfdcf0223ea229fcfc3b4d409a0ee59
Feodor Fitsner <feodor@appveyor.com>
2020-01-14 17:40:22 -0800

    Node versions updated

--- scripts/Windows/avvm/pack-node.cmd
+++ scripts/Windows/avvm/pack-node.cmd
@@ -1,11 +1,26 @@
-7z a c:\avvm\node\node-12.8.1-x86.7z c:\avvm\node\12.8.1\x86\*
-7z a c:\avvm\node\node-12.8.1-x64.7z c:\avvm\node\12.8.1\x64\*
+7z a c:\avvm\node\node-10.18.0-x86.7z c:\avvm\node\10.18.0\x86\*
+7z a c:\avvm\node\node-10.18.0-x64.7z c:\avvm\node\10.18.0\x64\*
 
-7z a c:\avvm\node\node-12.13.1-x86.7z c:\avvm\node\12.13.1\x86\*
-7z a c:\avvm\node\node-12.13.1-x64.7z c:\avvm\node\12.13.1\x64\*
+7z a c:\avvm\node\node-10.18.1-x86.7z c:\avvm\node\10.18.1\x86\*
+7z a c:\avvm\node\node-10.18.1-x64.7z c:\avvm\node\10.18.1\x64\*
 
-7z a c:\avvm\node\node-13.1.0-x86.7z c:\avvm\node\13.1.0\x86\*
-7z a c:\avvm\node\node-13.1.0-x64.7z c:\avvm\node\13.1.0\x64\*
+7z a c:\avvm\node\node-12.14.0-x86.7z c:\avvm\node\12.14.0\x86\*
+7z a c:\avvm\node\node-12.14.0-x64.7z c:\avvm\node\12.14.0\x64\*
 
-7z a c:\avvm\node\node-13.2.0-x86.7z c:\avvm\node\13.2.0\x86\*
-7z a c:\avvm\node\node-13.2.0-x64.7z c:\avvm\node\13.2.0\x64\*
+7z a c:\avvm\node\node-12.14.1-x86.7z c:\avvm\node\12.14.1\x86\*
+7z a c:\avvm\node\node-12.14.1-x64.7z c:\avvm\node\12.14.1\x64\*
+
+7z a c:\avvm\node\node-13.3.0-x86.7z c:\avvm\node\13.3.0\x86\*
+7z a c:\avvm\node\node-13.3.0-x64.7z c:\avvm\node\13.3.0\x64\*
+
+7z a c:\avvm\node\node-13.4.0-x86.7z c:\avvm\node\13.4.0\x86\*
+7z a c:\avvm\node\node-13.4.0-x64.7z c:\avvm\node\13.4.0\x64\*
+
+7z a c:\avvm\node\node-13.5.0-x86.7z c:\avvm\node\13.5.0\x86\*
+7z a c:\avvm\node\node-13.5.0-x64.7z c:\avvm\node\13.5.0\x64\*
+
+7z a c:\avvm\node\node-13.6.0-x86.7z c:\avvm\node\13.6.0\x86\*
+7z a c:\avvm\node\node-13.6.0-x64.7z c:\avvm\node\13.6.0\x64\*
+
+7z a c:\avvm\node\node-8.17.0-x86.7z c:\avvm\node\8.17.0\x86\*
+7z a c:\avvm\node\node-8.17.0-x64.7z c:\avvm\node\8.17.0\x64\*
--- scripts/Windows/avvm/test-node.cmd
+++ scripts/Windows/avvm/test-node.cmd
@@ -1,13 +1,28 @@
-c:\avvm\node\12.8.1\x86\nodejs\node --version
-c:\avvm\node\12.8.1\x64\nodejs\node --version
+c:\avvm\node\10.18.0\x86\nodejs\node --version
+c:\avvm\node\10.18.0\x64\nodejs\node --version
 
-c:\avvm\node\12.13.1\x86\nodejs\node --version
-c:\avvm\node\12.13.1\x64\nodejs\node --version
+c:\avvm\node\10.18.1\x86\nodejs\node --version
+c:\avvm\node\10.18.1\x64\nodejs\node --version
 
-c:\avvm\node\13.1.0\x86\nodejs\node --version
-c:\avvm\node\13.1.0\x64\nodejs\node --version
+c:\avvm\node\12.14.0\x86\nodejs\node --version
+c:\avvm\node\12.14.0\x64\nodejs\node --version
 
-c:\avvm\node\13.2.0\x86\nodejs\node --version
-c:\avvm\node\13.2.0\x64\nodejs\node --version
+c:\avvm\node\12.14.1\x86\nodejs\node --version
+c:\avvm\node\12.14.1\x64\nodejs\node --version
+
+c:\avvm\node\13.3.0\x86\nodejs\node --version
+c:\avvm\node\13.3.0\x64\nodejs\node --version
+
+c:\avvm\node\13.4.0\x86\nodejs\node --version
+c:\avvm\node\13.4.0\x64\nodejs\node --version
+
+c:\avvm\node\13.5.0\x86\nodejs\node --version
+c:\avvm\node\13.5.0\x64\nodejs\node --version
+
+c:\avvm\node\13.6.0\x86\nodejs\node --version
+c:\avvm\node\13.6.0\x64\nodejs\node --version
+
+c:\avvm\node\8.17.0\x86\nodejs\node --version
+c:\avvm\node\8.17.0\x64\nodejs\node --version
 
 pause
