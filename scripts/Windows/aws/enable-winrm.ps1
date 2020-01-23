59a7df1ed63878bda79e8a265b8924f799ff6c02
Feodor Fitsner <feodor@appveyor.com>
2020-01-22 20:28:08 -0800

    Modify enable-winrm.ps1

--- scripts/Windows/aws/enable-winrm.ps1
+++ scripts/Windows/aws/enable-winrm.ps1
@@ -1,34 +1,29 @@
 <powershell>
 
-write-output "Running User Data Script"
-write-host "(host) Running User Data Script"
-
-Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
-
-# Don't set this before Set-ExecutionPolicy as it throws an error
+# USERDATA SCRIPT FOR AMAZON SOURCE WINDOWS SERVER AMIS
+# BOOTSTRAPS WINRM VIA SSL
+ 
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
 $ErrorActionPreference = "stop"
-
-# Remove HTTP listener
+ 
+# Remove any existing Windows Management listeners
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
-
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
+ 
+# Create self-signed cert for encrypted WinRM on port 5986
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer-ami-builder"
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
-
-# WinRM
-write-output "Setting up WinRM"
-write-host "(host) setting up WinRM"
-
+ 
+# Configure WinRM
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
+cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
+cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="false"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
+cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
