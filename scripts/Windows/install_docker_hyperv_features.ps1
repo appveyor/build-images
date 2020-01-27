82702191d923a3695eb5d0c55d31f7dd2e593208
Feodor Fitsner <feodor@appveyor.com>
2020-01-26 17:44:06 -0800

    Install WSL separately

---
+++ scripts/Windows/install_docker_hyperv_features.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "Stop"
+
+# Containers feature
+
+$containersFeature = (Get-WindowsOptionalFeature -FeatureName Containers -Online)
+if ($containersFeature -and $containersFeature.State -ne 'Enabled') {
+	Write-Host "Installing Containers feature"
+	Enable-WindowsOptionalFeature -FeatureName Containers -Online -All -NoRestart
+}
+
+# Hyper-V feature
+
+if ((Get-WmiObject Win32_Processor).VirtualizationFirmwareEnabled[0] -and (Get-WmiObject Win32_Processor).SecondLevelAddressTranslationExtensions[0]) {
+	Write-Host "Installing Hyper-V feature"
+	Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+} else {
+	Write-Host "Skipping Hyper-V installation - virtualization is not enabled"
+}
