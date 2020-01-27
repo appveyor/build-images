9fe4bd3c7a12cd1bf54b330bf24df2c5a3c0bfc9
Feodor Fitsner <feodor@appveyor.com>
2020-01-27 12:27:27 -0800

    Return back azure_storage_account

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@
 # One can specify builders which should be build in variable $builders:
 # builders=googlecompute,hyperv-vmcx,hyperv-iso,amazon-ebs,virtualbox-iso,vmware-iso,azure-arm
 
-readonly AZURE_VARS="azure_client_id azure_client_secret azure_location azure_resource_group_name azure_subscription_id"
+readonly AZURE_VARS="azure_client_id azure_client_secret azure_location azure_resource_group_name azure_storage_account azure_subscription_id"
 readonly GCE_VARS="gce_account_file gce_project gce_zone"
 readonly AWS_VARS="aws_access_key aws_secret_key aws_region aws_security_group_id aws_subnet_id"
 readonly AWS_OPT_VARS="aws_ssh_keypair_name aws_ssh_private_key_file"
