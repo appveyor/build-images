25f32aaf61d469e52f48f849b35beb81b7357ff4
Feodor Fitsner <feodor@appveyor.com>
2020-02-10 10:48:45 -0800

    Read GitHub IPs from meta service endpoint

--- scripts/Windows/add_ssh_known_hosts.ps1
+++ scripts/Windows/add_ssh_known_hosts.ps1
@@ -91,14 +91,9 @@ if(-not (Test-Path $sshPath)) {
 $contents = @()
 # GitHub IP addresses
 
-$GIthubIPs="192.30.252.0/22",
-    "185.199.108.0/22",
-    "13.229.188.59/32",
-    "13.250.177.223/32",
-    "18.194.104.89/32",
-    "18.195.85.27/32",
-    "35.159.8.160/32",
-    "52.74.223.119/32"
+$gitHubMetaJson = (Invoke-WebRequest 'https://api.github.com/meta').Content
+$GIthubIPs = (ConvertFrom-Json $gitHubMetaJson).git
+
 Get-IPs -subnets $GIthubIPs | ForEach-Object {
     $contents += "github.com,$_ ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 }
