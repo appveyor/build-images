0925d57d65e217be4b2a1e371b86630d97f926bc
Feodor Fitsner <feodor@appveyor.com>
2020-02-25 16:07:02 -0800

    Update install_powershell_get.ps1

--- scripts/Windows/install_powershell_get.ps1
+++ scripts/Windows/install_powershell_get.ps1
@@ -2,7 +2,7 @@
 Write-Host "========================"
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name PowerShellGet -Force
+Install-Module -Name PowerShellGet -Force -AllowClobber
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
 
 Write-Host "PowerShellGet installed"
