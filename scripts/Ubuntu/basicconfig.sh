00e82f8b0023daa857f7f37aa1b37e8baa2ec6b6
Feodor Fitsner <feodor@appveyor.com>
2020-03-02 11:41:32 -0800

    Running script as

--- scripts/Ubuntu/basicconfig.sh
+++ scripts/Ubuntu/basicconfig.sh
@@ -22,6 +22,8 @@ SCRIPT_PID=$$
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root. Aborting." 1>&2
     exit 1
+else
+    echo "Running script as $(whoami)"
 fi
 
 case  ${PACKER_BUILDER_TYPE-} in
