a5289d1c2d89c8235d96c637dcf8b9a5996a44fa
Feodor Fitsner <feodor@appveyor.com>
2020-03-12 12:40:58 -0700

    Fix RunProcess, again

--- scripts/Windows/common.ps1
+++ scripts/Windows/common.ps1
@@ -1,20 +1,20 @@
 function DisplayDiskInfo() {
     Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName, 
     @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } }, 
-    @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f( $_.Size / 1gb)}}, 
-    @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f( $_.Freespace / 1gb ) } }, 
-    @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f( $_.FreeSpace / $_.Size ) } } |
+    @{ Name = "Size (GB)" ; Expression = { "{0:N1}" -f ( $_.Size / 1gb) } }, 
+    @{ Name = "FreeSpace (GB)" ; Expression = { "{0:N1}" -f ( $_.Freespace / 1gb ) } }, 
+    @{ Name = "PercentFree" ; Expression = { "{0:P1}" -f ( $_.FreeSpace / $_.Size ) } } |
     Format-Table -AutoSize | Out-String
 }
 
 function GetProductVersion ($partialName) {
     $x64items = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
     $x64items + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\CurrentVersion\Uninstall") `
-        | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
-        | Where-Object { $_.DisplayName -and $_.DisplayName.contains($partialName) } `
-        | Sort-Object -Property DisplayName `
-        | Select-Object -Property DisplayName,DisplayVersion `
-        | Format-Table -AutoSize | Out-String
+    | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
+    | Where-Object { $_.DisplayName -and $_.DisplayName.contains($partialName) } `
+    | Sort-Object -Property DisplayName `
+    | Select-Object -Property DisplayName, DisplayVersion `
+    | Format-Table -AutoSize | Out-String
 }
 
 function RunProcess($command) {
@@ -30,11 +30,7 @@ function RunProcess($command) {
 
     # find tool in path
     if (-not (Test-Path $fileName)) {
-        foreach($pathPart in $($env:PATH).Split(';')) {
-            $searchPath = [IO.Path]::Combine($pathPart, $fileName)
-            if (Test-Path $searchPath) {
-                $fileName = $searchPath; break;
-            }
+        foreach ($pathPart in $($env:PATH).Split(';')) {
             $searchPath = [IO.Path]::Combine($pathPart, "$fileName.cmd")
             if (Test-Path $searchPath) {
                 $fileName = $searchPath; break;
@@ -42,27 +38,44 @@ function RunProcess($command) {
             $searchPath = [IO.Path]::Combine($pathPart, "$fileName.exe")
             if (Test-Path $searchPath) {
                 $fileName = $searchPath; break;
-            }             
+            }
+            $searchPath = [IO.Path]::Combine($pathPart, $fileName)
+            if (Test-Path $searchPath) {
+                $fileName = $searchPath; break;
+            }
         }
     }
 
-    $fileName
-    $arguments
-    
-    $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo 
-    $ProcessInfo.FileName = $fileName
-    $ProcessInfo.RedirectStandardError = $true 
-    $ProcessInfo.RedirectStandardOutput = $true 
-    $ProcessInfo.UseShellExecute = $false 
-    $ProcessInfo.Arguments = $arguments
-    $Process = New-Object System.Diagnostics.Process 
-    $Process.StartInfo = $ProcessInfo 
-    $Process.Start() | Out-Null 
-    $Process.WaitForExit() 
-    $stdOut = $Process.StandardOutput.ReadToEnd()
-    $stdErr = $Process.StandardError.ReadToEnd()
-    $stdOut
-    if ($stdErr) {
-        "StdErr: $stdErr"
+    $psi = New-Object System.Diagnostics.ProcessStartInfo 
+    $psi.FileName = $fileName
+    $psi.RedirectStandardError = $true
+    $psi.RedirectStandardOutput = $true
+    $psi.CreateNoWindow = $true
+    $psi.UseShellExecute = $false
+    $psi.Arguments = $arguments
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $psi
+    $process.Start() | Out-Null
+
+    do {
+        if (!$process.StandardOutput.EndOfStream) {
+            Write-Host "$($process.StandardOutput.ReadLine())"
+        }
+        if (!$process.StandardError.EndOfStream) {
+            Write-Host "STDERR: $($process.StandardError.ReadLine())"
+        }
+        Start-Sleep -Milliseconds 10
+    } while (!$process.HasExited)
+
+    # read remainder
+    if (!$process.StandardOutput.EndOfStream) {
+        Write-Host "$($process.StandardOutput.ReadLine())"
+    }
+    if (!$process.StandardError.EndOfStream) {
+        Write-Host "STDERR: $($process.StandardError.ReadLine())"
+    }
+
+    if ($process.ExitCode -ne 0) {
+        $LASTEXITCODE = $process.ExitCode
     }
 }
