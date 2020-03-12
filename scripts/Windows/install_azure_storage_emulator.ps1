393c92abeca27c864f86cebcb6cd776fa04c30c3
Feodor Fitsner <feodor@appveyor.com>
2020-03-12 10:54:34 -0700

    Update Azure Storage Emulator on VS 2017 image

--- scripts/Windows/install_azure_storage_emulator.ps1
+++ scripts/Windows/install_azure_storage_emulator.ps1
@@ -1,8 +1,24 @@
-Write-Host "Installing Azure storage emulator 5.9..." -ForegroundColor Cyan
+function GetEmulatorVersion {
+    $x64items = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+    $x64items + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\CurrentVersion\Uninstall") `
+        | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
+        | Where-Object { $_.DisplayName -and $_.DisplayName.contains('Azure Storage Emulator') } `
+        | Sort-Object -Property DisplayName `
+        | Select-Object -Property DisplayName,DisplayVersion
+}
+
+GetEmulatorVersion
+
+Write-Host "Installing Azure storage emulator 5.10..." -ForegroundColor Cyan
+
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\MicrosoftAzureStorageEmulator.msi"
 (New-Object Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?LinkId=717179&clcid=0x409', $msiPath)
+
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /q
-del $msiPath
+Remove-Item $msiPath
+
+GetEmulatorVersion
+
 Write-Host "Installed Azure storage emulator" -ForegroundColor Green
