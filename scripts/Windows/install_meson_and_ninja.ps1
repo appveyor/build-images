6808e44a93d500bbdf3acb15f37cc11200f16fa1
Feodor Fitsner <feodor@appveyor.com>
2020-03-12 17:45:13 -0700

    install_meson_and_ninja

--- scripts/Windows/install_meson_and_ninja.ps1
+++ scripts/Windows/install_meson_and_ninja.ps1
@@ -2,7 +2,7 @@
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\meson.msi"
-(New-Object Net.WebClient).DownloadFile('https://github.com/mesonbuild/meson/releases/download/0.52.1/meson-0.52.1-64.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/mesonbuild/meson/releases/download/0.53.2/meson-0.53.2-64.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /quiet
