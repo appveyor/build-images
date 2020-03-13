abb9585a13739486c7ed108b661f23e92012d0f0
Feodor Fitsner <feodor@appveyor.com>
2020-03-13 16:39:30 -0700

    Fix PWSH installation

--- scripts/Windows/install_powershell_core.ps1
+++ scripts/Windows/install_powershell_core.ps1
@@ -21,7 +21,8 @@ if (Test-Path $appveyorPath) {
     }
     
     $pwshProfileFilename = "$pwshProfilePath\Microsoft.PowerShell_profile.ps1"
-    if ((Get-Content $pwshProfileFilename | Where-Object { $_.Contains("AppVeyor.BuildAgent.PowerShell.dll") }).Count -eq 0) {
+
+    if (-not (Test-Path $pwshProfileFilename) -or (Get-Content $pwshProfileFilename | Where-Object { $_.Contains("AppVeyor.BuildAgent.PowerShell.dll") }).Count -eq 0) {
         Write-Host "Updating $pwshProfileFilename with AppVeyor PS Modules"
         Add-Content $pwshProfileFilename "`nImport-Module '$appveyorPath\dotnetcore\AppVeyor.BuildAgent.PowerShell.dll'"
     } else {
