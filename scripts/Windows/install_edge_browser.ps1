c0320485ce16f9a21edad87929ecde55acc0976b
Feodor Fitsner <feodor@appveyor.com>
2020-03-13 12:14:04 -0700

    Update Firefox, Edge and Chrome browsers

--- scripts/Windows/install_edge_browser.ps1
+++ scripts/Windows/install_edge_browser.ps1
@@ -2,7 +2,7 @@
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\MicrosoftEdgeEnterpriseX64.msi"
-(New-Object Net.WebClient).DownloadFile('http://dl.delivery.mp.microsoft.com/filestreamingservice/files/c39f1d27-cd11-495a-b638-eac3775b469d/MicrosoftEdgeEnterpriseX64.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('http://dl.delivery.mp.microsoft.com/filestreamingservice/files/2828e59b-2d46-4c1d-8d52-235fe21da25b/MicrosoftEdgeEnterpriseX64.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /qn /norestart
