4fce46ba1283be3f695936321f8bca15a1a8717c
Feodor Fitsner <feodor@appveyor.com>
2020-03-13 10:22:42 -0700

    Service Fabric 7.0

--- scripts/Windows/install_service_fabric.ps1
+++ scripts/Windows/install_service_fabric.ps1
@@ -1,16 +1,26 @@
-﻿# install runtime
-$exePath = "$env:TEMP\MicrosoftServiceFabric-Runtime.exe"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/5/5/555B653A-4893-4FBD-A256-3CFC555D626E/MicrosoftServiceFabric.6.3.162.9494.exe', $exePath)
+﻿Write-Host "Installing Service Fabric 7.0" -ForegroundColor Cyan
+
+# install runtime
+Write-Host "Downloading Service Fabric Runtime..."
+$exePath = "$env:TEMP\MicrosoftServiceFabricRuntime.exe"
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/0/0/f/00fbca28-0a64-4c9a-a3a3-b11763ee17e5/MicrosoftServiceFabric.7.0.470.9590.exe', $exePath)
+
+Write-Host "Installing Service Fabric Runtime..."
 cmd /c start /wait $exePath /AcceptEULA
-del $exePath
+Remove-Item $exePath
 
 # install SDK and VS Tools
-if (test-path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community") {
+if ((test-path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community") -or
+        (test-path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community")) {
     # SDK only
+    Write-Host "Installing Service Fabric SDK..."
     cmd /c start /wait webpicmd /Install /Products:MicrosoftAzure-ServiceFabric-CoreSDK /AcceptEula
 } else {
     # SDK and Tools
+    Write-Host "Installing Service Fabric SDK and Tools..."
     cmd /c start /wait webpicmd /Install /Products:MicrosoftAzure-ServiceFabric-VS2015 /AcceptEula
 }
 
-del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Service Fabric Local Cluster Manager.lnk"
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Service Fabric Local Cluster Manager.lnk" -Force
+
+Write-Host "Service Fabric installed" -ForegroundColor Green
---
+++ scripts/Windows/uninstall_service_fabric.ps1
@@ -0,0 +1,33 @@
+﻿. "$PSScriptRoot\common.ps1"
+
+Write-Host "Uninstalling Service Fabric" -ForegroundColor Cyan
+
+function GetUninstallString($productName) {
+    $x64items = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+    $x64userItems = @(Get-ChildItem "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+    ($x64items + $x64userItems + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\CurrentVersion\Uninstall") `
+        | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
+        | Where-Object { $_.DisplayName -and $_.DisplayName -eq $productName } `
+        | Select-Object UninstallString).UninstallString
+}
+
+Write-Host "Uninstalling Service Fabric SDK..."
+
+$sdkUninstallString = GetUninstallString "Microsoft Azure Service Fabric SDK"
+if ($sdkUninstallString) {
+    $uninstallCommand = $sdkUninstallString.replace('MsiExec.exe /I{', '/x{').replace('MsiExec.exe /X{', '/x{')
+    cmd /c start /wait msiexec.exe $uninstallCommand /quiet
+} else {
+    Write-Host "Service Fabric SDK not found" -ForegroundColor Yellow
+}
+
+Write-Host "Uninstalling Service Fabric Runtime..."
+
+$runtimeUninstallString = GetUninstallString "Microsoft Azure Service Fabric"
+if ($runtimeUninstallString) {
+     Start-ProcessWithOutput $runtimeUninstallString
+} else {
+    Write-Host "Service Fabric Runtime not found" -ForegroundColor Yellow
+}
+
+Write-Host "Service Fabric uninstalled" -ForegroundColor Green
