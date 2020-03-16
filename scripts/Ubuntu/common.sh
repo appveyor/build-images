0a33d44f09627d8c0a9789b346c5323ff16dc96f
Feodor Fitsner <feodor@appveyor.com>
2020-03-16 14:58:16 -0700

    get microsoft.asc

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -1468,6 +1468,9 @@ function install_azurecli() {
     AZ_REPO=${OS_CODENAME}
     sudo apt-get -y -q install ca-certificates curl apt-transport-https gnupg
 
+    wget https://packages.microsoft.com/keys/microsoft.asc
+    ls
+
     curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
         gpg --dearmor | 
         sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
