04980b99cbd9120a25e253a82afb7bb3d106b0c5
Feodor Fitsner <feodor@appveyor.com>
2020-03-15 17:26:24 -0700

    install_sql_server_2019

--- scripts/Windows/install_sql_server_2019.ps1
+++ scripts/Windows/install_sql_server_2019.ps1
@@ -1,13 +1,13 @@
-﻿Write-Host "Downloading SQL Server 2017..."
-$isoPath = "$env:TEMP\SQLServer2017-x64-ENU-Dev.iso"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLServer2017-x64-ENU-Dev.iso', $isoPath)
+﻿Write-Host "Downloading SQL Server 2019..."
+$isoPath = "$env:TEMP\SQLServer2019-x64-ENU-Dev.iso"
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-x64-ENU-Dev.iso', $isoPath)
 
-$extractPath = "$env:TEMP\SQLServer2017-x64-ENU-Dev"
+$extractPath = "$env:TEMP\SQLServer2019-x64-ENU-Dev"
 Write-Host "Extracting..."
 7z x $isoPath -aoa -o"$extractPath" | Out-Null
 
 Write-Host "Installing..."
-cmd /c start /wait $extractPath\setup.exe /q /ACTION=Install /FEATURES=SQLEngine,FullText,RS /INSTANCENAME=SQL2017 /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS" /TCPENABLED=1 /SECURITYMODE=SQL /SAPWD=Password12! /IACCEPTSQLSERVERLICENSETERMS
+cmd /c start /wait $extractPath\setup.exe /q /ACTION=Install /FEATURES=SQLEngine,FullText,RS /INSTANCENAME=SQL2019 /SQLSYSADMINACCOUNTS="BUILTIN\ADMINISTRATORS" /TCPENABLED=1 /SECURITYMODE=SQL /SAPWD=Password12! /IACCEPTSQLSERVERLICENSETERMS
 
 Write-Host "Deleting temporary files..."
 Remove-Item $isoPath -Force -ErrorAction Ignore
@@ -16,8 +16,8 @@ Remove-Item $extractPath -Recurse -Force -ErrorAction Ignore
 Write-Host "OK"
 
 Write-Host "Downloading SQL Server Reporting Services..."
-$reportingPath = "$env:TEMP\SQLServerReportingServices2017.exe"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/E/6/4/E6477A2A-9B58-40F7-8AD6-62BB8491EA78/SQLServerReportingServices.exe', $reportingPath)
+$reportingPath = "$env:TEMP\SQLServerReportingServices2019.exe"
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/1/a/a/1aaa9177-3578-4931-b8f3-373b24f63342/SQLServerReportingServices.exe', $reportingPath)
 
 Write-Host "Installing..."
 cmd /c start /wait $reportingPath /quiet /norestart /IACCEPTLICENSETERMS /Edition=Dev
@@ -25,28 +25,28 @@ cmd /c start /wait $reportingPath /quiet /norestart /IACCEPTLICENSETERMS /Editio
 Write-Host "Deleting temporary files..."
 Remove-Item $reportingPath -Force -ErrorAction Ignore
 
-Write-Host "Preparing SQL Server 2017..."
+Write-Host "Preparing SQL Server 2019..."
 
 Write-Host "Stopping services..."
 
-Stop-Service 'MSSQL$SQL2017'
-Stop-Service 'MSSQLFDLauncher$SQL2017'
-Stop-Service 'SQLAgent$SQL2017'
-Stop-Service 'MSOLAP$SQL2017' -ErrorAction Ignore
-Stop-Service 'SSASTELEMETRY$SQL2017' -ErrorAction Ignore
-Stop-Service 'SQLTELEMETRY$SQL2017'
+Stop-Service 'MSSQL$SQL2019'
+Stop-Service 'MSSQLFDLauncher$SQL2019'
+Stop-Service 'SQLAgent$SQL2019'
+Stop-Service 'MSOLAP$SQL2019' -ErrorAction Ignore
+Stop-Service 'SSASTELEMETRY$SQL2019' -ErrorAction Ignore
+Stop-Service 'SQLTELEMETRY$SQL2019'
 Stop-Service 'SQLBrowser'
 Stop-Service 'SQLServerReportingServices'
 Stop-Service 'SQLWriter'
 
 Write-Host "Changing services startup mode..."
 
-Set-Service 'MSSQL$SQL2017' -StartupType Manual
-Set-Service 'MSSQLFDLauncher$SQL2017' -StartupType Manual
-Set-Service 'SQLAgent$SQL2017' -StartupType Manual
-Set-Service 'MSOLAP$SQL2017' -StartupType Manual -ErrorAction Ignore
-Set-Service 'SSASTELEMETRY$SQL2017' -StartupType Manual -ErrorAction Ignore
-Set-Service 'SQLTELEMETRY$SQL2017' -StartupType Manual
+Set-Service 'MSSQL$SQL2019' -StartupType Manual
+Set-Service 'MSSQLFDLauncher$SQL2019' -StartupType Manual
+Set-Service 'SQLAgent$SQL2019' -StartupType Manual
+Set-Service 'MSOLAP$SQL2019' -StartupType Manual -ErrorAction Ignore
+Set-Service 'SSASTELEMETRY$SQL2019' -StartupType Manual -ErrorAction Ignore
+Set-Service 'SQLTELEMETRY$SQL2019' -StartupType Manual
 Set-Service 'SQLBrowser' -StartupType Manual
 Set-Service 'SQLServerReportingServices' -StartupType Manual
 Set-Service 'SQLWriter' -StartupType Manual
@@ -54,7 +54,7 @@ Set-Service 'SQLWriter' -StartupType Manual
 Write-Host "Updating SQL Server TCP/IP configuration..."
 
 Import-Module "sqlps" -DisableNameChecking -ErrorAction SilentlyContinue 3> $null
-$instanceName = 'SQL2017'
+$instanceName = 'SQL2019'
 $computerName = $env:COMPUTERNAME
 $smo = 'Microsoft.SqlServer.Management.Smo.'
 $wmi = New-Object ($smo + 'Wmi.ManagedComputer')
@@ -82,4 +82,5 @@ Stop-Service "MSSQL`$$instanceName"
 Stop-Service "MSSQLFDLauncher`$$instanceName" -ErrorAction SilentlyContinue
 Stop-Service SQLBrowser
 
-Write-Host "SQL Server 2017 installed and configured" -ForegroundColor Green
+Write-Host "SQL Server 2019 installed and configured" -ForegroundColor Green
+
