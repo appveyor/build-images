82ba5478539500cfa56ce88211bfed43875130c3
Feodor Fitsner <feodor@appveyor.com>
2020-04-22 13:30:01 -0700

    Pythons version bump and cleanup

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -683,7 +683,7 @@ function install_pip() {
 
 function install_pythons(){
     command -v virtualenv || install_virtualenv
-    declare PY_VERSIONS=( "2.6.9" "2.7.17" "3.4.10" "3.5.9" "3.6.10" "3.7.0" "3.7.1" "3.7.2" "3.7.3" "3.7.4" "3.7.5" "3.7.7" "3.8.0" "3.8.1" "3.8.2" "3.9.0a4" )
+    declare PY_VERSIONS=( "2.6.9" "2.7.18" "3.4.10" "3.5.9" "3.6.10" "3.7.7" "3.8.2" "3.9.0a5" )
     for i in "${PY_VERSIONS[@]}"; do
         VENV_PATH=${HOME}/venv${i%%[abrcf]*}
         VENV_MINOR_PATH=${HOME}/venv${i%.*}
