03ce066a84eb198e6afb9129cb6fea13438bbb8f
Feodor Fitsner <feodor@appveyor.com>
2020-04-28 18:56:31 -0700

    Install .NET only

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -783,17 +783,18 @@ function preheat_dotnet_sdks() {
 }
 
 function prepare_dotnet_packages() {
-    declare SDK_VERSIONS=( "2.1" "2.2" "3.0" "3.1" )
+    #shellcheck disable=SC2034
+    SDK_VERSIONS=( "2.0.0" "2.0.2" "2.0.3" "2.1.2" "2.1.3" "2.1.4" "2.1.101" "2.1.103" "2.1.104" "2.1.105" "2.1.200" "2.1.201" "2.1.202" "2.1" "2.2" "3.0" "3.1" )
     dotnet_packages "dotnet-sdk-" SDK_VERSIONS[@]
-
-    declare RUNTIME_VERSIONS=( "2.1" "2.2" )
+    #shellcheck disable=SC2034
+    declare RUNTIME_VERSIONS=( "2.0.0" "2.0.3" "2.0.4" "2.0.5" "2.0.6" "2.0.7" "2.0.9" "2.1" "2.2" )
     dotnet_packages "dotnet-runtime-" RUNTIME_VERSIONS[@]
-
-    declare RUNTIME_VERSIONS=( "2.1" "2.2" )
+    #shellcheck disable=SC2034
+    declare RUNTIME_VERSIONS=( "2.1" "2.2" "3.0" )
     dotnet_packages "aspnetcore-runtime-" RUNTIME_VERSIONS[@]
-
-    # required by Ubuntu 16.04 for .NET SDK 1.1.14
-    PACKAGES+=( "libunwind-dev" )
+    #shellcheck disable=SC2034
+    declare DEV_VERSIONS=( "1.1.5" "1.1.6" "1.1.7" "1.1.8" "1.1.9" "1.1.10" "1.1.11" "1.1.12" "1.1.12" "1.1.13" "1.1.14" )
+    dotnet_packages "dotnet-dev-" DEV_VERSIONS[@]
 }
 
 function config_dotnet_repository() {
--- scripts/Ubuntu/minimalconfig.sh
+++ scripts/Ubuntu/minimalconfig.sh
@@ -141,7 +141,14 @@ install_gitlfs ||
 update_git ||
     _abort $?
 
-install_browsers ||
+# .NET stuff
+install_dotnets ||
+    _abort $?
+install_dotnetv5_preview ||
+    _abort $?
+preheat_dotnet_sdks &&
+log_version dotnet --list-sdks &&
+log_version dotnet --list-runtimes ||
     _abort $?
 # ====================================
 
