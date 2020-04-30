e2859c4de25a683d0210391b23617c071475f762
Feodor Fitsner <feodor@appveyor.com>
2020-04-29 21:40:49 -0700

    try removing from cron

--- scripts/Ubuntu/focal.sh
+++ scripts/Ubuntu/focal.sh
@@ -16,11 +16,17 @@ function disable_automatic_apt_updates() {
     systemd-run --property="After=apt-daily.service apt-daily-upgrade.service" --wait /bin/true
     apt-get -y purge unattended-upgrades
     apt-get -y remove update-notifier update-notifier-common
+    apt-get -y install python
+    sudo rm /etc/cron.daily/update-notifier-common
 
     echo 'APT::Periodic::Update-Package-Lists "0";
 APT::Periodic::Download-Upgradeable-Packages "0";
 APT::Periodic::AutocleanInterval "0";
 APT::Periodic::Unattended-Upgrade "0";' > /etc/apt/apt.conf.d/20auto-upgrades
+
+    echo 'APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";' > /etc/apt/apt.conf.d/10periodic
 }
 
 function add_releasespecific_tools() {
