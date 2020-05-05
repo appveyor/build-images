06a438c71d5ed5583c0f304b51e47ab8d69b95f9
Feodor Fitsner <feodor@appveyor.com>
2020-05-05 13:10:24 -0700

    Fix disable apt-daily.service

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -274,18 +274,12 @@ function disable_automatic_apt_updates() {
     systemctl stop apt-daily.service
     systemctl kill --kill-who=all apt-daily.service
 
-    # wait until `apt-get updated` has been killed
-    while ! (systemctl list-units --all apt-daily.service | egrep -q '(dead|failed)')
-    do
-        sleep 1;
-    done
-
     systemctl disable apt-daily.service
     systemctl disable apt-daily.timer
     systemctl disable apt-daily-upgrade.timer    
 
-    #systemctl daemon-reload
-    #systemd-run --property="After=apt-daily.service apt-daily-upgrade.service" --wait /bin/true
+    systemctl daemon-reload
+    systemd-run --property="After=apt-daily.service apt-daily-upgrade.service" --wait /bin/true
     apt-get -y purge unattended-upgrades
 #     apt-get -y remove update-notifier update-notifier-common
 #     apt-get -y install python
