f25ac6b7b2644ad5d3816ba1fdd0a28624325294
Feodor Fitsner <feodor@appveyor.com>
2020-08-28 09:50:44 -0700

    Microsoft Edge 85.0.564.41

--- scripts/Windows/install_edge_browser.ps1
+++ scripts/Windows/install_edge_browser.ps1
@@ -4,7 +4,7 @@ Write-Host "Installing Microsoft Edge..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\MicrosoftEdgeEnterpriseX64.msi"
-(New-Object Net.WebClient).DownloadFile('http://dl.delivery.mp.microsoft.com/filestreamingservice/files/6937705c-f53d-45d3-8a09-f4ebbc0ecf10/MicrosoftEdgeEnterpriseX64.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('http://dl.delivery.mp.microsoft.com/filestreamingservice/files/0083d4f9-6b4f-4bcc-9c6e-838705f33904/MicrosoftEdgeEnterpriseX64.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /qn /norestart
