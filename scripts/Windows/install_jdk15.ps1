6da9756062aa9c01b6b158fec4c7b818365a4519
Feodor Fitsner <feodor@appveyor.com>
2020-11-10 19:12:33 -0800

    JDK 15

---
+++ scripts/Windows/install_jdk15.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "Installing JDK 15 ..." -ForegroundColor Cyan
+
+New-Item "${env:ProgramFiles}\Java" -ItemType Directory -Force | Out-Null
+
+$jdkPath = "${env:ProgramFiles}\Java\jdk15"
+
+if(Test-Path $jdkPath) {
+    Remove-Item $jdkPath -Recurse -Force
+}
+
+Write-Host "Downloading..."
+$zipPath = "$env:TEMP\openjdk-15_windows-x64_bin.zip"
+(New-Object Net.WebClient).DownloadFile('https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_windows-x64_bin.zip', $zipPath)
+
+Write-Host "Unpacking..."
+$tempPath = "$env:TEMP\jdk15_temp"
+7z x $zipPath -o"$tempPath" | Out-Null
+[IO.Directory]::Move("$tempPath\jdk-15.0.1", $jdkPath)
+Remove-Item $tempPath -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $zipPath -ErrorAction SilentlyContinue
+
+cmd /c "`"$jdkPath\bin\java`" --version"
+
+if ($env:INSTALL_LATEST_ONLY) {
+    Add-Path "$jdkPath\bin"
+}
+
+Write-Host "JDK 15 installed" -ForegroundColor Green
