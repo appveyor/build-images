c7549eb2b8ace9964e76ff7af13c58153e0d1be4
Feodor Fitsner <feodor@appveyor.com>
2020-12-13 14:25:05 -0800

    Fix Qt 6 installation

--- scripts/Windows/install_qt_module.ps1
+++ scripts/Windows/install_qt_module.ps1
@@ -54,12 +54,19 @@ if ($isLinux) {
 $package_updates = @{}
 $feeds_cache = @{}
 
+function GetQtPrefix($version) {
+    if ($version.startsWith('6')) {
+        return 'qt6'
+    } else {
+        return 'qt5'
+    }
+}
 function GetVersionId($version) {
     return $version.replace('.', '')
 }
 
 function GetReleaseRootUrl($version) {
-    return "$QT_ROOT_URL/qt5_$(GetVersionId $version)"
+    return "$QT_ROOT_URL/$(GetQtPrefix $version)_$(GetVersionId $version)"
 }
 
 function FetchToolsUpdatePackages($toolsId) {
@@ -136,7 +143,7 @@ function Install-QtComponent {
 
     if ($Version -and $Name) {
         FetchReleaseUpdatePackages $version
-        InstallComponentById "qt.qt5.$(GetVersionId $version).$Name" $Path -whatif:$whatIf -excludeDocs:$excludeDocs -excludeExamples:$excludeExamples
+        InstallComponentById "qt.$(GetQtPrefix $version).$(GetVersionId $version).$Name" $Path -whatif:$whatIf -excludeDocs:$excludeDocs -excludeExamples:$excludeExamples
     } elseif ($Id) {
         InstallComponentById $Id $Path -whatif:$whatIf -excludeDocs:$excludeDocs -excludeExamples:$excludeExamples
     } else {
