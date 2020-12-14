711a7f60d6f9460500f61c5a0d118f57c7a731a4
Feodor Fitsner <feodor@appveyor.com>
2020-12-13 19:45:36 -0800

    Fix Go 1.14.13 and build part II only

--- scripts/Windows/install_go.ps1
+++ scripts/Windows/install_go.ps1
@@ -10,7 +10,7 @@ $go_versions = @(
 if (-not $env:INSTALL_LATEST_ONLY) {
     $go_versions = $go_versions + @(
         @{
-            "version" = "1.14.14"
+            "version" = "1.14.13"
             "folder" = "go114"
         }
         @{
