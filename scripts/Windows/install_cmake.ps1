bfef37071c758830f1352280e7ea8e46a7154702
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2021-04-12 05:20:09 +0200

    Update CMake URLs for 3.20 release (#76)
    * Update CMake URLs for 3.20 release
    
    Also now installs the 64-bit version of CMake on Windows
    
    See https://cmake.org/cmake/help/latest/release/3.20.html#id19
    
    * Windows: Update CMake paths for 64-bit install

--- scripts/Windows/install_cmake.ps1
+++ scripts/Windows/install_cmake.ps1
@@ -1,6 +1,6 @@
 ﻿$cmakeVersion = "3.20.1"
 
-$cmakeUninstallPath = "${env:ProgramFiles(x86)}\CMake\Uninstall.exe"
+$cmakeUninstallPath = "${env:ProgramFiles}\CMake\Uninstall.exe"
 if([IO.File]::Exists($cmakeUninstallPath)) {
     Write-Host "Uninstalling previous CMake ..." -ForegroundColor Cyan
     # uninstall existent
@@ -11,17 +11,17 @@ if([IO.File]::Exists($cmakeUninstallPath)) {
 }
 
 Write-Host "Installing CMake $cmakeVersion ..." -ForegroundColor Cyan
-$msiPath = "$env:TEMP\cmake-$cmakeVersion-win32-x86.msi"
+$msiPath = "$env:TEMP\cmake-$cmakeVersion-windows-x86_64.msi"
 
 Write-Host "Downloading..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(New-Object Net.WebClient).DownloadFile("https://github.com/Kitware/CMake/releases/download/v$cmakeVersion/cmake-$cmakeVersion-win32-x86.msi", $msiPath)
+(New-Object Net.WebClient).DownloadFile("https://github.com/Kitware/CMake/releases/download/v$cmakeVersion/cmake-$cmakeVersion-windows-x86_64.msi", $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i $msiPath /quiet
 Remove-Item $msiPath
 
-Add-Path "${env:ProgramFiles(x86)}\CMake\bin"
+Add-Path "${env:ProgramFiles}\CMake\bin"
 
 remove-path 'C:\ProgramData\chocolatey\bin'
 add-path 'C:\ProgramData\chocolatey\bin'
