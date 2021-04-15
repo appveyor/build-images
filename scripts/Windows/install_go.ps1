5f61fd73f529b746735cf327ed035e8d94a7e005
Feodor Fitsner <feodor@appveyor.com>
2021-04-15 16:28:40 -0700

    Go 1.16.3, 1.15.11

--- scripts/Windows/install_go.ps1
+++ scripts/Windows/install_go.ps1
@@ -2,7 +2,7 @@
 
 $go_versions = @(
     @{
-        "version" = "1.16.2"
+        "version" = "1.16.3"
         "folder" = "go116"
     }
 )
@@ -10,7 +10,7 @@ $go_versions = @(
 if (-not $env:INSTALL_LATEST_ONLY) {
     $go_versions = $go_versions + @(
         @{
-            "version" = "1.15.10"
+            "version" = "1.15.11"
             "folder" = "go115"
         }        
         @{
