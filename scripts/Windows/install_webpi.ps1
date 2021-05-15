1943de197c475081b8c4271bc3019b61698daa65
Feodor Fitsner <feodor@appveyor.com>
2021-05-15 15:28:09 -0700

    Fix Web PI installer

--- scripts/Windows/install_webpi.ps1
+++ scripts/Windows/install_webpi.ps1
@@ -10,7 +10,7 @@ if([IO.File]::Exists("$webPIFolder\webpicmd.exe")) {
 
 # http://www.iis.net/learn/install/web-platform-installer/web-platform-installer-direct-downloads
 $msiPath = "$env:TEMP\WebPlatformInstaller_amd64_en-US.msi"
-(New-Object Net.WebClient).DownloadFile('http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?LinkId=287166', $msiPath)
 
 cmd /c start /wait msiexec /i "$msiPath" /q
 del $msiPath
