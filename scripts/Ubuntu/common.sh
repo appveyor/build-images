e551298b147b4f8dec9a58894a6121337aaa73b3
Feodor Fitsner <feodor@appveyor.com>
2021-06-02 10:27:09 -0700

    Python 3.7.10, 3.8.10, 3.9.5

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -707,7 +707,7 @@ function install_pip() {
 
 function install_pythons(){
     command -v virtualenv || install_virtualenv
-    declare PY_VERSIONS=( "2.6.9" "2.7.18" "3.4.10" "3.5.10" "3.6.13" "3.7.9" "3.8.9" "3.9.4" )
+    declare PY_VERSIONS=( "2.6.9" "2.7.18" "3.4.10" "3.5.10" "3.6.13" "3.7.10" "3.8.10" "3.9.5" )
     for i in "${PY_VERSIONS[@]}"; do
         VENV_PATH=${HOME}/venv${i%%[abrcf]*}
         VENV_MINOR_PATH=${HOME}/venv${i%.*}
