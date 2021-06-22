a3aa8ca3c170eaa6acbf3b127aab1c7283c63379
JSJVESKN28 <83856241+JSJVESKN28@users.noreply.github.com>
2021-06-22 03:57:11 -0300

    Update ec2-userdata.ps1

--- scripts/Windows/ec2-userdata.ps1
+++ scripts/Windows/ec2-userdata.ps1
@@ -11,7 +11,7 @@ $ErrorActionPreference = "stop"
 # Remove HTTP listener
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
 
-Set-Item WSMan:\localhost\MaxTimeoutms 1800000
+Set-Item WSMan:\localhost\MaxTimeoutms -1
 Set-Item WSMan:\localhost\Service\Auth\Basic $true
 
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
