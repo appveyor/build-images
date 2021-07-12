a06d5bab507343eab6c27ed9efc505684ce0b56f
Feodor Fitsner <feodor@appveyor.com>
2021-07-11 17:11:21 -0700

    Get Bitbucket IPs from JSON

--- scripts/Windows/add_ssh_known_hosts.ps1
+++ scripts/Windows/add_ssh_known_hosts.ps1
@@ -99,11 +99,8 @@ Get-IPs -subnets $GIthubIPs | ForEach-Object {
 }
 
 # BitBucket
-$BitBucketIPs = @(
-    "18.205.93.0/25",
-    "18.234.32.128/25",
-    "13.52.5.0/25"
-    )
+$bitbucketHubMetaJson = (Invoke-WebRequest 'https://ip-ranges.atlassian.com/').Content
+$bitbucketIPs = (ConvertFrom-Json $bitbucketHubMetaJson).items.cidr | Where-Object { $_.indexOf(':') -eq -1 }
 Get-IPs -subnets $BitBucketIPs | ForEach-Object {
     $contents += "bitbucket.org,$_ ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw=="
 }
