078241b64f8437ac8fe9f67fa571a8b4b88cdfc1
Feodor Fitsner <feodor@appveyor.com>
2021-08-04 12:37:19 -0700

    Update selenium drivers

--- scripts/Windows/install_selenium_drivers.ps1
+++ scripts/Windows/install_selenium_drivers.ps1
@@ -12,7 +12,7 @@ Add-Path $destPath
 Write-Host "Installing Chrome Selenium driver..." -ForegroundColor Cyan
 
 $zipPath = "$env:TEMP\chromedriver_win32.zip"
-(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_win32.zip', $zipPath)
+(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_win32.zip', $zipPath)
 7z x $zipPath -aoa -o"$destPath"
 Remove-Item $zipPath
 
