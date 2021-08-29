85fc927458933c310ae88c91c6050dc8389af018
Feodor Fitsner <feodor@appveyor.com>
2021-08-29 14:25:33 -0700

    Python 3.6.14, 3.7.11, 3.8.11, 3.9.6

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -707,7 +707,7 @@ function install_pip() {
 
 function install_pythons(){
     command -v virtualenv || install_virtualenv
-    declare PY_VERSIONS=( "2.6.9" "2.7.18" "3.4.10" "3.5.10" "3.6.13" "3.7.10" "3.8.10" "3.9.5" )
+    declare PY_VERSIONS=( "2.6.9" "2.7.18" "3.4.10" "3.5.10" "3.6.14" "3.7.11" "3.8.11" "3.9.6" )
     for i in "${PY_VERSIONS[@]}"; do
         VENV_PATH=${HOME}/venv${i%%[abrcf]*}
         VENV_MINOR_PATH=${HOME}/venv${i%.*}
