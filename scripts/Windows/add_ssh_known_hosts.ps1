7108e8b26a48f4960bed8c797c282ad1b11ad0b9
Feodor Fitsner <feodor@appveyor.com>
2021-08-29 18:32:53 -0700

    Filter ipv6 for GitHub

--- scripts/Windows/add_ssh_known_hosts.ps1
+++ scripts/Windows/add_ssh_known_hosts.ps1
@@ -90,11 +90,10 @@ if(-not (Test-Path $sshPath)) {
 
 $contents = @()
 # GitHub IP addresses
-
 $gitHubMetaJson = (Invoke-WebRequest 'https://api.github.com/meta').Content
-$GIthubIPs = (ConvertFrom-Json $gitHubMetaJson).git
+$GithubIPs = (ConvertFrom-Json $gitHubMetaJson).git | Where-Object { $_.indexOf(':') -eq -1 }
 
-Get-IPs -subnets $GIthubIPs | ForEach-Object {
+Get-IPs -subnets $GithubIPs | ForEach-Object {
     $contents += "github.com,$_ ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 }
 
