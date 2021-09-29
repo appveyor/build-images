ff0732661551a26ccf163f940d7712cac973dc52
Feodor Fitsner <feodor@appveyor.com>
2021-09-29 12:33:58 -0700

    Windows 11 SDK/WDK (10.0.22000.1)

---
+++ scripts/Windows/install_winsdk_22000.ps1
@@ -0,0 +1,20 @@
+Write-Host "Installing Windows 11 SDK (10.0.22000.1)..." -ForegroundColor Cyan
+
+Write-Host "Downloading..."
+$isoPath = "$env:TEMP\sdksetup.iso"
+(New-Object Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=2166460', $isoPath)
+
+$extractPath = "$env:TEMP\sdksetup22000"
+Write-Host "Extracting..."
+7z x $isoPath -aoa -o"$extractPath" | Out-Null
+
+Write-Host "Installing..."
+cmd /c start /wait $extractPath\WinSDKSetup.exe /features + /quiet
+
+Write-Host "Deleting temporary files..."
+Remove-Item $isoPath -Force -ErrorAction Ignore
+Remove-Item $extractPath -Recurse -Force -ErrorAction Ignore
+
+dir "C:\Program Files (x86)\Windows Kits\10\bin"
+
+Write-Host "Installed" -ForegroundColor Green
