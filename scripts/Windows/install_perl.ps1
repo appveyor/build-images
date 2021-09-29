9bccf4c1a21eccef043f32c2e8624533d022b2ec
Feodor Fitsner <feodor@appveyor.com>
2021-09-29 11:03:34 -0700

    Strawberry Perl 5.32.1.1

--- scripts/Windows/install_perl.ps1
+++ scripts/Windows/install_perl.ps1
@@ -1,9 +1,9 @@
-Write-Host "Installing Strawberry Perl 5.30..." -ForegroundColor Cyan
+Write-Host "Installing Strawberry Perl 5.32.1.1..." -ForegroundColor Cyan
 
-$msiPath = "$env:TEMP\strawberry-perl-5.30.2.1-64bit.msi"
+$msiPath = "$env:TEMP\strawberry-perl-5.32.1.1-64bit.msi"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('http://strawberryperl.com/download/5.30.2.1/strawberry-perl-5.30.2.1-64bit.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-64bit.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /qn /norestart
