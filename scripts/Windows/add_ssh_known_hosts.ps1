1595b4b68ca658aa3f501bd6797fd87dd69b451c
Feodor Fitsner <feodor@appveyor.com>
2021-10-20 09:33:57 -0700

    Update optimized add_ssh_known_hosts.ps1 to include new GitHub keys

--- scripts/Windows/add_ssh_known_hosts.ps1
+++ scripts/Windows/add_ssh_known_hosts.ps1
@@ -1,12 +1,16 @@
 function Get-IPs {
 
-        Param(
-        [Parameter(Mandatory = $true)]
-        [array] $Subnets
-        )
+    Param(
+    [Parameter(Mandatory = $true)]
+    [array] $Subnets
+    )
 
-foreach ($subnet in $subnets)
+    foreach ($subnet in $subnets)
     {
+        $ip1 = ''
+        $ip2 = ''
+        $ip3 = ''
+
         #Split IP and subnet
         $IP = ($Subnet -split "\/")[0]
         $SubnetBits = ($Subnet -split "\/")[1]
@@ -45,38 +49,45 @@ foreach ($subnet in $subnets)
 
             #Next ID is first network ID converted to decimal plus $i then converted to binary
             For ($i = 1 ; $i -le $imax ; $i++)
-                {
-                    #Convert to decimal and add $i
-                    $NextHostIDInDecimal = ([convert]::ToInt32($HostIDInBinary,2) + $i)
-                    #Convert back to binary
-                    $NextHostIDInBinary = [convert]::ToString($NextHostIDInDecimal,2)
-                    #Add leading zeros
-                    #Number of zeros to add
-                    $NoOfZerosToAdd = $HostIDInBinary.Length - $NextHostIDInBinary.Length
-                    $NextHostIDInBinary = ("0" * $NoOfZerosToAdd) + $NextHostIDInBinary
+            {
+                #Convert to decimal and add $i
+                $NextHostIDInDecimal = ([convert]::ToInt32($HostIDInBinary,2) + $i)
+                #Convert back to binary
+                $NextHostIDInBinary = [convert]::ToString($NextHostIDInDecimal,2)
+                #Add leading zeros
+                #Number of zeros to add
+                $NoOfZerosToAdd = $HostIDInBinary.Length - $NextHostIDInBinary.Length
+                $NextHostIDInBinary = ("0" * $NoOfZerosToAdd) + $NextHostIDInBinary
 
-                    #Work out next IP
-                    #Add networkID to hostID
-                    $NextIPInBinary = $NetworkIDInBinary + $NextHostIDInBinary
-                    #Split into octets and separate by . then join
-                    $IP = @()
-                    For ($x = 1 ; $x -le 4 ; $x++)
-                        {
-                            #Work out start character position
-                            $StartCharNumber = ($x-1)*8
-                            #Get octet in binary
-                            $IPOctetInBinary = $NextIPInBinary.Substring($StartCharNumber,8)
-                            #Convert octet into decimal
-                            $IPOctetInDecimal = [convert]::ToInt32($IPOctetInBinary,2)
-                            #Add octet to IP
-                            $IP += $IPOctetInDecimal
-                        }
+                #Work out next IP
+                #Add networkID to hostID
+                $NextIPInBinary = $NetworkIDInBinary + $NextHostIDInBinary
+                #Split into octets and separate by . then join
+                $IP = @()
+                For ($x = 1 ; $x -le 4 ; $x++)
+                    {
+                        #Work out start character position
+                        $StartCharNumber = ($x-1)*8
+                        #Get octet in binary
+                        $IPOctetInBinary = $NextIPInBinary.Substring($StartCharNumber,8)
+                        #Convert octet into decimal
+                        $IPOctetInDecimal = [convert]::ToInt32($IPOctetInBinary,2)
+                        #Add octet to IP
+                        $IP += $IPOctetInDecimal
+                    }
+
+                if ($ip1 -ne $IP[0] -or $ip2 -ne $IP[1] -or $ip3 -ne $IP[2]) {
+                    $IP[3] = '*'
+                    $ip1 = $IP[0]
+                    $ip2 = $IP[1]
+                    $ip3 = $IP[2]
 
                     #Separate by .
                     $IP = $IP -join "."
                     $IPs += $IP
                 }
-            $IPs
+            }
+            $IPs -join ","
         }
     }
 }
@@ -95,6 +106,8 @@ $GithubIPs = (ConvertFrom-Json $gitHubMetaJson).git | Where-Object { $_.indexOf(
 
 Get-IPs -subnets $GithubIPs | ForEach-Object {
     $contents += "github.com,$_ ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+    $contents += "github.com,$_ ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+    $contents += "github.com,$_ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl"
 }
 
 # BitBucket
@@ -105,9 +118,9 @@ Get-IPs -subnets $BitBucketIPs | ForEach-Object {
 }
 
 # GitLab
-$contents += "gitlab.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf"
-$contents += "gitlab.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsj2bNKTBSpIYDEGk9KxsGh3mySTRgMtXL583qmBpzeQ+jqCMRgBqB98u3z++J1sKlXHWfM9dyhSevkMwSbhoR8XIq/U0tCNyokEi/ueaBMCvbcTHhO7FcwzY92WK4Yt0aGROY5qX2UKSeOvuP4D6TPqKF1onrSzH9bx9XUf2lEdWT/ia1NEKjunUqu1xOB/StKDHMoX4/OKyIzuS0q/T1zOATthvasJFoPrAjkohTyaDUz2LN5JoH839hViyEG82yB+MjcFV5MU3N1l1QL3cVUCh93xSaua1N85qivl+siMkPGbO5xR/En4iEY6K2XPASUEMaieWVNTRCtJ4S8H+9"
-$contents += "gitlab.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFSMqzJeV9rUzU4kWitGjeR4PWSa29SPqJ1fVkhtj3Hw9xjLVXVYrU9QlYWrOLXBpQ6KWjbjTDTdDkoohFzgbEY="
+$contents += "gitlab.com,* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf"
+$contents += "gitlab.com,* ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsj2bNKTBSpIYDEGk9KxsGh3mySTRgMtXL583qmBpzeQ+jqCMRgBqB98u3z++J1sKlXHWfM9dyhSevkMwSbhoR8XIq/U0tCNyokEi/ueaBMCvbcTHhO7FcwzY92WK4Yt0aGROY5qX2UKSeOvuP4D6TPqKF1onrSzH9bx9XUf2lEdWT/ia1NEKjunUqu1xOB/StKDHMoX4/OKyIzuS0q/T1zOATthvasJFoPrAjkohTyaDUz2LN5JoH839hViyEG82yB+MjcFV5MU3N1l1QL3cVUCh93xSaua1N85qivl+siMkPGbO5xR/En4iEY6K2XPASUEMaieWVNTRCtJ4S8H+9"
+$contents += "gitlab.com,* ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFSMqzJeV9rUzU4kWitGjeR4PWSa29SPqJ1fVkhtj3Hw9xjLVXVYrU9QlYWrOLXBpQ6KWjbjTDTdDkoohFzgbEY="
 
 $knownhostfile = Join-Path $sshPath "known_hosts"
 [IO.File]::WriteAllLines($knownhostfile, $contents)
