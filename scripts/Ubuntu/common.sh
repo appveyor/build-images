c6500e62d7acbe96a96f2cc3311448442a732fea
Feodor Fitsner <feodor@appveyor.com>
2021-11-12 14:09:39 -0800

    CMake 3.21.4

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -1534,7 +1534,7 @@ function install_cmake() {
     echo "[INFO] Running install_cmake..."
     local VERSION
     if [[ -z "${1-}" || "${#1}" = "0" ]]; then
-        VERSION=3.21.2
+        VERSION=3.21.4
     else
         VERSION=$1
     fi
