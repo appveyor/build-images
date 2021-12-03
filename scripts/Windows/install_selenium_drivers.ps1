0250598efefc5eeef0997b33179420a8c0bcf53c
Feodor Fitsner <feodor@appveyor.com>
2021-12-03 13:11:37 -0800

    Googe Chrome 96.0.4664.45

--- scripts/Windows/install_selenium_drivers.ps1
+++ scripts/Windows/install_selenium_drivers.ps1
@@ -12,7 +12,7 @@ Add-Path $destPath
 Write-Host "Installing Chrome Selenium driver..." -ForegroundColor Cyan
 
 $zipPath = "$env:TEMP\chromedriver_win32.zip"
-(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/95.0.4638.54/chromedriver_win32.zip', $zipPath)
+(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_win32.zip', $zipPath)
 7z x $zipPath -aoa -o"$destPath"
 Remove-Item $zipPath
 
