0899cfd17157380d396827bf111736be2684bdec
Feodor Fitsner <feodor@appveyor.com>
2021-12-03 13:03:30 -0800

    Go 1.17.4, 1.16.11

--- scripts/Windows/install_go.ps1
+++ scripts/Windows/install_go.ps1
@@ -2,7 +2,7 @@
 
 $go_versions = @(
     @{
-        "version" = "1.17.2"
+        "version" = "1.17.4"
         "folder" = "go117"
     }
 )
@@ -10,7 +10,7 @@ $go_versions = @(
 if (-not $env:INSTALL_LATEST_ONLY) {
     $go_versions = $go_versions + @(
         @{
-            "version" = "1.16.9"
+            "version" = "1.16.11"
             "folder" = "go116"
         }        
         @{
