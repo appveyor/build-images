0903213bc362dce81ed69845b6f506576e9a6f40
Feodor Fitsner <feodor@appveyor.com>
2022-02-21 15:33:02 -0800

    XCode 13.2.1

--- scripts/macos/common.sh
+++ scripts/macos/common.sh
@@ -609,7 +609,7 @@ function install_xcode() {
 
     # big sur
     if [ "$OSX_MAJOR_VER" -eq 11 ]; then
-        XCODE_VERSIONS=( "12.5.1" "13" )
+        XCODE_VERSIONS=( "12.5.1" "13.2.1" )
     fi
 
     #check fastlane
