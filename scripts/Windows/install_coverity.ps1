4b3c8a18685ff27b5d377016611c3a76ca188ad4
Feodor Fitsner <feodor@appveyor.com>
2022-03-02 21:21:58 -0800

    Coverity Scan 2021.12

--- scripts/Windows/install_coverity.ps1
+++ scripts/Windows/install_coverity.ps1
@@ -6,14 +6,14 @@ if (Test-Path $destPath ) {
 }
 
 Write-Host "Downloading..."
-$zipPath = "$env:TEMP\cov-analysis-win64-2019.03.zip"
-(New-Object Net.WebClient).DownloadFile('https://storage.googleapis.com/appveyor-download-cache/coverity/cov-analysis-win64-2019.03.zip', $zipPath)
+$zipPath = "$env:TEMP\cov-analysis-win64-2021.12.zip"
+(New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/cov-analysis-win64-2021.12.zip', $zipPath)
 
 Write-Host "Unpacking..."
 $tempPath = "$env:TEMP\Coverity"
 7z x $zipPath -o"$tempPath" | Out-Null
 
-[IO.Directory]::Move("$tempPath\cov-analysis-win64-2019.03", $destPath)
+[IO.Directory]::Move("$tempPath\cov-analysis-win64-2021.12.1", $destPath)
 del $tempPath -Recurse -Force
 del $zipPath
 
