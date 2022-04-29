e45335badf46488e5bbccb429c452c8a4a911a77
Feodor Fitsner <feodor@appveyor.com>
2022-04-28 20:16:39 -0700

    Fix mingw64 and re-build part 2

--- scripts/Windows/install_mingw-w64.ps1
+++ scripts/Windows/install_mingw-w64.ps1
@@ -11,12 +11,12 @@ New-Item $mingwPath -ItemType Directory -Force | Out-Null
 
 $destPath = "$mingwPath\x86_64-8.1.0-posix-seh-rt_v6-rev0"
 $zipPath = "$env:TEMP\mingw-w64.7z"
-(New-Object Net.WebClient).DownloadFile('https://phoenixnap.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z', $zipPath)
+(New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z', $zipPath)
 7z x $zipPath -o"$destPath" -aoa
 Remove-Item $zipPath
 
 $destPath = "$mingwPath\i686-8.1.0-posix-dwarf-rt_v6-rev0"
-(New-Object Net.WebClient).DownloadFile('https://phoenixnap.dl.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z', $zipPath)
+(New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z', $zipPath)
 7z x $zipPath -o"$destPath" -aoa
 Remove-Item $zipPath
 
