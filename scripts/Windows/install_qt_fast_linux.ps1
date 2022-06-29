6804a62c7c43e72182991b12eae2d7ba2b2db8dd
Feodor Fitsner <feodor@appveyor.com>
2022-06-29 12:36:34 -0700

    Qt 6.3.1, 6.4.0 - added, 6.1 - removed
    Qt

--- scripts/Windows/install_qt_fast_linux.ps1
+++ scripts/Windows/install_qt_fast_linux.ps1
@@ -6,7 +6,7 @@ $installDir = "$env:HOME/Qt"
 
 $component_groups = @(
     @{
-        version    = "6.3.0"
+        version    = "6.4.0"
         components = @(
             "gcc_64",
             "debug_info",
@@ -15,18 +15,50 @@ $component_groups = @(
             "addons.qt3d.gcc_64",
             "addons.qtcharts",
             "addons.qtcharts.gcc_64",
+            "addons.qtconnectivity",
+            "addons.qtconnectivity.gcc_64",                
             "addons.qtdatavis3d",
             "addons.qtdatavis3d.gcc_64",
+            "addons.qthttpserver",
+            "addons.qthttpserver.gcc_64",            
             "addons.qtimageformats",
             "addons.qtimageformats.gcc_64",
+            "addons.qtlanguageserver",
+            "addons.qtlanguageserver.gcc_64",                 
             "addons.qtlottie",
             "addons.qtlottie.gcc_64",
+            "addons.qtmultimedia",
+            "addons.qtmultimedia.gcc_64",                
             "addons.qtnetworkauth",
             "addons.qtnetworkauth.gcc_64",
+            "addons.qtpdf",
+            "addons.qtpdf.gcc_64",            
+            "addons.qtpositioning",
+            "addons.qtpositioning.gcc_64",            
+            "addons.qtquick3dphysics",
+            "addons.qtquick3dphysics.gcc_64",
+            "addons.qtremoteobjects",
+            "addons.qtremoteobjects.gcc_64",                             
             "addons.qtscxml",
             "addons.qtscxml.gcc_64",
+            "addons.qtsensors",
+            "addons.qtsensors.gcc_64",            
+            "addons.qtserialbus",
+            "addons.qtserialbus.gcc_64",
+            "addons.qtserialport",
+            "addons.qtserialport.gcc_64",            
+            "addons.qtspeech",
+            "addons.qtspeech.gcc_64",              
             "addons.qtvirtualkeyboard",
-            "addons.qtvirtualkeyboard.gcc_64",
+            "addons.qtvirtualkeyboard.gcc_64",            
+            "addons.qtwebchannel",
+            "addons.qtwebchannel.gcc_64",            
+            "addons.qtwebengine",
+            "addons.qtwebengine.gcc_64", 
+            "addons.qtwebsockets",
+            "addons.qtwebsockets.gcc_64",       
+            "addons.qtwebview",
+            "addons.qtwebview.gcc_64", 
             "qt5compat",
             "qt5compat.gcc_64",        
             "qtquick3d",
@@ -38,11 +70,45 @@ $component_groups = @(
             "qtwaylandcompositor",
             "qtwaylandcompositor.gcc_64"            
         )
-    }
+    }      
 )
 
 if (-not $env:INSTALL_LATEST_ONLY) {
     $component_groups += @(
+        @{
+            version    = "6.3.1"
+            components = @(
+                "gcc_64",
+                "debug_info",
+                "debug_info.gcc_64",
+                "addons.qt3d",
+                "addons.qt3d.gcc_64",
+                "addons.qtcharts",
+                "addons.qtcharts.gcc_64",
+                "addons.qtdatavis3d",
+                "addons.qtdatavis3d.gcc_64",
+                "addons.qtimageformats",
+                "addons.qtimageformats.gcc_64",
+                "addons.qtlottie",
+                "addons.qtlottie.gcc_64",
+                "addons.qtnetworkauth",
+                "addons.qtnetworkauth.gcc_64",
+                "addons.qtscxml",
+                "addons.qtscxml.gcc_64",
+                "addons.qtvirtualkeyboard",
+                "addons.qtvirtualkeyboard.gcc_64",
+                "qt5compat",
+                "qt5compat.gcc_64",        
+                "qtquick3d",
+                "qtquick3d.gcc_64",
+                "qtquicktimeline",
+                "qtquicktimeline.gcc_64",                     
+                "qtshadertools",
+                "qtshadertools.gcc_64",
+                "qtwaylandcompositor",
+                "qtwaylandcompositor.gcc_64"            
+            )
+        }        
         @{
             version    = "6.2.4"
             components = @(
@@ -77,40 +143,6 @@ if (-not $env:INSTALL_LATEST_ONLY) {
                 "qtwaylandcompositor.gcc_64"            
             )
         }        
-        @{
-            version    = "6.1.3"
-            components = @(
-                "gcc_64",
-                "debug_info",
-                "debug_info.gcc_64",
-                "addons.qt3d",
-                "addons.qt3d.gcc_64",
-                "addons.qtcharts",
-                "addons.qtcharts.gcc_64",
-                "addons.qtdatavis3d",
-                "addons.qtdatavis3d.gcc_64",
-                "addons.qtimageformats",
-                "addons.qtimageformats.gcc_64",
-                "addons.qtlottie",
-                "addons.qtlottie.gcc_64",
-                "addons.qtnetworkauth",
-                "addons.qtnetworkauth.gcc_64",
-                "addons.qtscxml",
-                "addons.qtscxml.gcc_64",
-                "addons.qtvirtualkeyboard",
-                "addons.qtvirtualkeyboard.gcc_64",
-                "qt5compat",
-                "qt5compat.gcc_64",        
-                "qtquick3d",
-                "qtquick3d.gcc_64",
-                "qtquicktimeline",
-                "qtquicktimeline.gcc_64",                     
-                "qtshadertools",
-                "qtshadertools.gcc_64",
-                "qtwaylandcompositor",
-                "qtwaylandcompositor.gcc_64"            
-            )
-        }            
         @{
             version    = "5.15.2"
             components = @(
@@ -149,7 +181,7 @@ if (-not $env:INSTALL_LATEST_ONLY) {
 $component_groups += @(
     @{
         components = @(
-            "qt.tools.ifw.43",
+            "qt.tools.ifw.44",
             "qt.license.thirdparty"
         )
     }
@@ -172,9 +204,9 @@ foreach ($componentGroup in $component_groups) {
 
 # set aliases
 ln -s "$HOME/Qt/5.15.2" "$HOME/Qt/latest"
-ln -s "$HOME/Qt/6.3.0" "$HOME/Qt/6.3"
+ln -s "$HOME/Qt/6.4.0" "$HOME/Qt/6.4"
+ln -s "$HOME/Qt/6.3.1" "$HOME/Qt/6.3"
 ln -s "$HOME/Qt/6.2.4" "$HOME/Qt/6.2"
-ln -s "$HOME/Qt/6.1.3" "$HOME/Qt/6.1"
 ln -s "$HOME/Qt/5.15.2" "$HOME/Qt/5.15"
 
 Write-Host "Qt 5.x installed" -ForegroundColor Green
