6e33d2ea786d86b89706d0ea7ee761a748b178f0
Owen McDonnell <omcdonnell@gmail.com>
2023-04-19 11:12:36 -0700

    Fix Qt symlinks

--- scripts/Windows/install_qt_fast_windows.ps1
+++ scripts/Windows/install_qt_fast_windows.ps1
@@ -605,8 +605,8 @@ Write-Host "OK" -ForegroundColor Green
 # set aliases
 $sym_links = @{
     "latest" = "5.15.2"
+    "6.5"    = "6.5.0"
     "6.4"    = "6.4.3"    
-    "6.3"    = "6.3.2"
     "6.2"    = "6.2.4"
     "5.15"   = "5.15.2"
     "5.9"    = "5.9.9"
