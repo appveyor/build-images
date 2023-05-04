83ef532d00559408e8e6ed5d9632ce5d38e48665
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-05-04 16:42:18 -0700

    fix msipath variable spelling error

--- scripts/Windows/install_sqlserver_odbc_driver.ps1
+++ scripts/Windows/install_sqlserver_odbc_driver.ps1
@@ -5,8 +5,8 @@ $msiPath = "$env:TEMP\msodbcsql.msi"
 (New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/c/5/4/c54c2bf1-87d0-4f6f-b837-b78d34d4d28a/en-US/18.2.1.1/x64/msodbcsql.msi', $msiPath)
 
 Write-Host "Installing..."
-cmd /c start /wait msiexec /quiet /passive /qn /i $msiPat IACCEPTMSODBCSQLLICENSETERMS=YES ADDLOCAL=ALL
-Remove-Item $msiPath
+cmd /c start /wait msiexec /quiet /passive /qn /i $msiPath IACCEPTMSODBCSQLLICENSETERMS=YES ADDLOCAL=ALL
+#Remove-Item $msiPath
 
 Write-Host "ODBC version 18 installed" -ForegroundColor Green
 
