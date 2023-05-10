c67919e5462015435fdc0ed50bce87e5e5274b2a
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-05-09 23:04:02 -0700

    change install script order

--- scripts/Windows/install_sqlserver_odbc_driver.ps1
+++ scripts/Windows/install_sqlserver_odbc_driver.ps1
@@ -28,15 +28,14 @@ $msiPath = "$env:TEMP\msodbcsql.msi"
 (New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/c/5/4/c54c2bf1-87d0-4f6f-b837-b78d34d4d28a/en-US/18.2.1.1/x64/msodbcsql.msi', $msiPath)
 
 Write-Host "Installing..."
-cmd /c start /wait msiexec /quiet /passive /qn /i $msiPath IACCEPTMSODBCSQLLICENSETERMS=YES ADDLOCAL=ALL
-#Remove-Item $msiPath
+cmd /c start /wait msiexec /quiet /passive /qn /i $msiPath IACCEPTMSODBCSQLLICENSETERMS=YES
+Remove-Item $msiPath -Force -ErrorAction Ignore
 
+# Write-Host "Installing SQLCMD utility..." -ForegroundColor Cyan
+# $msiPath = "$env:TEMP\MsSqlCmdLnUtils.msi"
+# (New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/a/a/4/aa47b3b0-9f67-441d-8b00-e74cd845ea9f/EN/x64/MsSqlCmdLnUtils.msi', $msiPath)
 
-Write-Host "Installing SQLCMD utility..." -ForegroundColor Cyan
-$msiPath = "$env:TEMP\MsSqlCmdLnUtils.msi"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/a/a/4/aa47b3b0-9f67-441d-8b00-e74cd845ea9f/EN/x64/MsSqlCmdLnUtils.msi', $msiPath)
-
-cmd /c start /wait msiexec /passive /qn /i $msiPath
+# cmd /c start /wait msiexec /passive /qn /i $msiPath
 
 
 Write-Host "ODBC version 18 installed" -ForegroundColor Green
