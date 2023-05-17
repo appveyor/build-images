9ee5db6e320041affefa1a760b12cf6c0fc2134d
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-05-16 22:01:00 -0700

    Sql server 2022 (#131)
    * add sql server 2022 install script
    
    * enable named pipes on sql server installation
    
    * disable named pipes
    
    * try enable again
    
    * add odbc driver install script
    
    * disable gce run
    
    * block rdp
    
    * try install odbc 18 without uninstalling 17
    
    * fix script spelling
    
    * manual check for odbc 18
    
    * try different install options
    
    * try different install options
    
    * fix msipath variable spelling error
    
    * uninstall ODBC 17 first
    
    * remove rdp block
    
    * check odbc 17 removal manually
    
    * fix yml parse error
    
    * install with ODBC features enabled
    
    * remove blocking
    
    * install with ODBC feature enabled
    
    * install with 'tools' flag
    
    * install with 'ADV_SSMS' flag
    
    * install with 'Client-Tools-Connectivity' flag
    
    * install with 'SSMS' flag
    
    * try ODBC driver update then tools flag
    
    * switch driver sql server install order
    
    * install sqlcmd utility
    
    * remove named pipes flag on sql server 2022 install
    
    * change install script order
    
    * skip odbc driver install
    
    * cleanup
    
    * bake GCE image
    
    * set registry value for sql server port
    
    * install new odbc driver
    
    * remove odbc driver update script
    
    * bake GCE image
    
    * Add sql server 2022 post install configuration script
    
    * only Hyper-v
    
    * GCE bake

---
+++ scripts/Windows/install_sqlserver_odbc_driver.ps1
@@ -0,0 +1,35 @@
+function GetUninstallString($productName) {
+    $x64items = @(Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+    $uninstallString = ($x64items + @(Get-ChildItem "HKLM:SOFTWARE\wow6432node\Microsoft\Windows\CurrentVersion\Uninstall") `
+        | ForEach-object { Get-ItemProperty Microsoft.PowerShell.Core\Registry::$_ } `
+        | Where-Object { $_.DisplayName -and $_.DisplayName -eq $productName } `
+        | Select UninstallString).UninstallString
+
+    if ($uninstallString) {
+        return $uninstallString.replace('MsiExec.exe /I{', '/x{').replace('MsiExec.exe /X{', '/x{')
+    }
+    else {
+        return $uninstallString
+    }
+}
+
+$odbc17Name = "Microsoft ODBC Driver 17 for SQL Server"
+$uninstallCommand = (GetUninstallString $odbc17Name)
+if ($uninstallCommand) {
+    Write-Host "Uninstalling $odbc17Name..."
+    cmd /c start /wait msiexec.exe $uninstallCommand /quiet   
+}
+
+Write-Host "Installing ODBC driver 18..." -ForegroundColor Cyan
+
+Write-Host "Downloading..."
+$msiPath = "$env:TEMP\msodbcsql.msi"
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/c/5/4/c54c2bf1-87d0-4f6f-b837-b78d34d4d28a/en-US/18.2.1.1/x64/msodbcsql.msi', $msiPath)
+
+Write-Host "Installing..."
+cmd /c start /wait msiexec /quiet /passive /qn /i $msiPath IACCEPTMSODBCSQLLICENSETERMS=YES
+Remove-Item $msiPath -Force -ErrorAction Ignore
+
+Write-Host "ODBC version 18 installed" -ForegroundColor Green
+
+
