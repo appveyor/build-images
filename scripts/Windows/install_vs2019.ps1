5910d6b040f7e2ed80b519dd199982d694501057
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-06-16 10:13:00 -0700

    Try previous VS 2019 pin

--- scripts/Windows/install_vs2019.ps1
+++ scripts/Windows/install_vs2019.ps1
@@ -268,7 +268,7 @@ if ($env:install_vs2019_preview) {
 	$VSBootstrapperURL = 'https://aka.ms/vs/16/pre/vs_community.exe'
 } else {
 	Write-Host "Installing from 'Release' channel"
-	$VSBootstrapperURL = 'https://aka.ms/vs/16/release/vs_community.exe'
+	#$VSBootstrapperURL = 'https://aka.ms/vs/16/release/vs_community.exe'
 	#$ChannelUri = 'https://aka.ms/vs/16/release/112851321_818166240/channel'
 
 	# This is how to know channelUri for previous versions of VS 2019
@@ -280,8 +280,8 @@ if ($env:install_vs2019_preview) {
 	# https://aka.ms/vs/16/release/149189645_1152370582/channel is the url to `VisualStudio.16.Release.chman` file.
 
 	# Pin VS 2019 16.5.5 for now because of issues with devenv.com: https://developercommunity.visualstudio.com/content/problem/1048804/cannot-build-project-with-devenvcom-in-visual-stud.html
-	#$ChannelUri = 'https://aka.ms/vs/16/release/149189645_1152370582/channel'
-	#$VSBootstrapperURL = 'https://download.visualstudio.microsoft.com/download/pr/68d6b204-9df0-4fcc-abcc-08ee0eff9cb2/b029547488a9383b0c8d8a9c813e246feb3ec19e0fe55020d4878fde5f0983fe/vs_Community.exe'
+	$ChannelUri = 'https://aka.ms/vs/16/release/149189645_1152370582/channel'
+	$VSBootstrapperURL = 'https://download.visualstudio.microsoft.com/download/pr/68d6b204-9df0-4fcc-abcc-08ee0eff9cb2/b029547488a9383b0c8d8a9c813e246feb3ec19e0fe55020d4878fde5f0983fe/vs_Community.exe'
 }
 
 $ErrorActionPreference = 'Stop'
