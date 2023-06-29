9af25bbe2ca00d74e23aa21113a3566eebb15c6e
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-06-29 13:43:12 -0700

    InnoSetup 6.2.2

--- scripts/Windows/install_innosetup.ps1
+++ scripts/Windows/install_innosetup.ps1
@@ -1,9 +1,9 @@
-﻿Write-Host "Installing InnoSetup 6.2.0..." -ForegroundColor Cyan
+﻿Write-Host "Installing InnoSetup 6.2.2..." -ForegroundColor Cyan
 
-$exePath = "$env:TEMP\innosetup-6.2.0.exe"
+$exePath = "$env:TEMP\innosetup-6.2.2.exe"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('https://files.jrsoftware.org/is/6/innosetup-6.2.0.exe', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://files.jrsoftware.org/is/6/innosetup-6.2.2.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
