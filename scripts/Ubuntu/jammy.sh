b10687ff233fd377ea962c1e1bc8c8c772341df8
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-08-07 17:27:40 -0700

    cat docker.list

--- scripts/Ubuntu/jammy.sh
+++ scripts/Ubuntu/jammy.sh
@@ -64,6 +64,8 @@ function configure_docker_repository() {
         "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
         tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    cat /etc/apt/sources.list.d/docker.list
 }
 
 function install_nvm_nodejs() {
