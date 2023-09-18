5e99898933b7fc38c1eb4f0246b5b2a7751a886b
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-09-18 15:41:15 -0700

    remove gpg command

--- scripts/Ubuntu/jammy.sh
+++ scripts/Ubuntu/jammy.sh
@@ -152,7 +152,7 @@ function configure_mono_repository () {
     echo "[INFO] Running configure_mono_repository on Ubuntu 22.04..."
     
     sudo apt-get install ca-certificates gnupg
-    sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    #sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
     sudo apt update
 
