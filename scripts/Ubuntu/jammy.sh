3b5e1ca734df1bf4ad9586dc255e9ede6b12848f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-09-29 10:42:01 -0700

    re-add preferences, remove dotnet runtimes

--- scripts/Ubuntu/jammy.sh
+++ scripts/Ubuntu/jammy.sh
@@ -20,13 +20,13 @@ function prepare_dotnet_packages() {
     SDK_VERSIONS=("3.1" "6.0" "7.0" )
     dotnet_packages "dotnet-sdk-" SDK_VERSIONS[@]
 
-    RUNTIME_VERSIONS=( "2.1" "2.2" )
-    dotnet_packages "dotnet-runtime-" RUNTIME_VERSIONS[@]
+    # RUNTIME_VERSIONS=( "2.1" "2.2" )
+    # dotnet_packages "dotnet-runtime-" RUNTIME_VERSIONS[@]
 }
 
 function config_dotnet_repository() {
-    #touch /etc/apt/preferences
-    #echo -e 'Package: *\nPin: origin "packages.microsoft.com"\nPin-Priority: 1001' | sudo tee /etc/apt/preferences
+    touch /etc/apt/preferences
+    echo -e 'Package: *\nPin: origin "packages.microsoft.com"\nPin-Priority: 1001' | sudo tee /etc/apt/preferences
     curl -fsSL -O https://packages.microsoft.com/config/ubuntu/${OS_RELEASE}/packages-microsoft-prod.deb &&
     dpkg -i packages-microsoft-prod.deb &&
     apt-get -y -q update ||
