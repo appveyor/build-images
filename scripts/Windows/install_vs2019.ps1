3d2f23b0feec86fc47b12d36ba7a7833e0b4d439
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-10-05 09:54:06 -0700

    remove android vs components

--- scripts/Windows/install_vs2019.ps1
+++ scripts/Windows/install_vs2019.ps1
@@ -70,16 +70,16 @@ Function InstallVS
   }
 }
 
-$WorkLoads = '--add Component.Android.NDK.R16B ' + `
-	'--add Component.Android.SDK25.Private ' + `
-	'--add Component.Android.SDK28 ' + `
-	'--add Component.Linux.CMake ' + `
-	'--add Component.MDD.Android ' + `
+$WorkLoads = '--add Component.Linux.CMake ' + `
+	#'--add Component.Android.SDK25.Private ' + `
+	#'--add Component.Android.SDK28 ' + `
+	#'--add Component.Android.NDK.R16B ' + `
+	#'--add Component.MDD.Android ' + `
 	'--add Component.MDD.Linux ' + `
 	'--add Component.Microsoft.VisualStudio.RazorExtension ' + `
 	'--add Component.Microsoft.VisualStudio.Web.AzureFunctions ' + `
 	'--add Component.Microsoft.Web.LibraryManager ' + `
-#	'--add Component.OpenJDK ' + `
+	'--add Component.OpenJDK ' + `
 	'--add Component.Xamarin ' + `
 	'--add Component.Xamarin.RemotedSimulator ' + `
 	'--add Microsoft.Component.Azure.DataLake.Tools ' + `
