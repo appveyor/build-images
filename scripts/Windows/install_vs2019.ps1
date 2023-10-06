bd73adbebab233378aa0aa367c59b0186c4b26a0
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-10-06 09:04:05 -0700

    restore vs2019 install script

--- scripts/Windows/install_vs2019.ps1
+++ scripts/Windows/install_vs2019.ps1
@@ -70,11 +70,11 @@ Function InstallVS
   }
 }
 
-$WorkLoads = '--add Component.Linux.CMake ' + `
-	#'--add Component.Android.SDK25.Private ' + `
-	#'--add Component.Android.SDK28 ' + `
-	#'--add Component.Android.NDK.R16B ' + `
-	#'--add Component.MDD.Android ' + `
+$WorkLoads = '--add Component.Android.NDK.R16B ' + `
+	'--add Component.Android.SDK25.Private ' + `
+	'--add Component.Android.SDK28 ' + `
+	'--add Component.Linux.CMake ' + `
+	'--add Component.MDD.Android ' + `
 	'--add Component.MDD.Linux ' + `
 	'--add Component.Microsoft.VisualStudio.RazorExtension ' + `
 	'--add Component.Microsoft.VisualStudio.Web.AzureFunctions ' + `
