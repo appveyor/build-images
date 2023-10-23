a4429ee40d3fc4967415c9db586bfa8d50262f35
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-10-22 23:17:06 -0700

    QT 6.5.3

--- scripts/Windows/install_qt_fast_windows.ps1
+++ scripts/Windows/install_qt_fast_windows.ps1
@@ -6,7 +6,7 @@ $installDir = "C:\Qt"
 
 $component_groups = @(
     @{
-        version    = "6.5.2"
+        version    = "6.5.3"
         components = @(
             "win64_mingw",
             "win64_msvc2019_64",
