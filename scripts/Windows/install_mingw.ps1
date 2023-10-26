e0bc8432370de3f09827fd8f764e6feefdd32140
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-10-25 20:51:05 -0700

    try invoke-webrequest

--- scripts/Windows/install_mingw.ps1
+++ scripts/Windows/install_mingw.ps1
@@ -11,6 +11,7 @@ if(Test-Path $mingwPath) {
 $zipPath = "$env:TEMP\mingw-get-0.6.3-mingw32-pre-20170905-1-bin.zip"
 Write-Host "Downloading..."
 (New-Object Net.WebClient).DownloadFile('https://osdn.net/frs/redir.php?m=plug&f=mingw%2F68260%2Fmingw-get-0.6.3-mingw32-pre-20170905-1-bin.zip', $zipPath)
+Invoke-WebRequest -Uri 'https://osdn.net/frs/redir.php?m=plug&f=mingw%2F68260%2Fmingw-get-0.6.3-mingw32-pre-20170905-1-bin.zip' -OutFile $zipPath -SkipCertificateCheck
 #(New-Object Net.WebClient).DownloadFile('https://osdn.net/projects/mingw/downloads/68260/mingw-get-0.6.3-mingw32-pre-20170905-1-bin.zip', $zipPath)
 Push-Location -Path $zipPath
 Write-Host "Unzipping..."
