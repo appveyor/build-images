fcaa38752aa75409f0b5e561ced22f68769fa3fa
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-11-27 16:35:34 -0800

    try new android sdk tools and add licenses accept

--- scripts/Windows/install_android_sdk.ps1
+++ scripts/Windows/install_android_sdk.ps1
@@ -12,7 +12,8 @@ $sdkPath = "$env:temp\android-sdk"
 $licenseZipPath = "$env:temp\android-sdk-licenses.zip"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile("https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip", $zipPath)
+#(New-Object Net.WebClient).DownloadFile("https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip", $zipPath)
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/android/repository/commandlinetools-win-10406996_latest.zip", $zipPath)
 if (-not (Test-Path $zipPath)) { throw "Unable to find $zipPath" }
 7z x $zipPath -aoa -o"$sdkPath"
 Remove-Item $zipPath -Force -ErrorAction Ignore
@@ -34,7 +35,7 @@ if (Test-Path $ndk_root) {
 setx ANDROID_HOME $sdk_root /M
 
 Push-Location -Path $sdkPath
-
+& '.\tools\bin\sdkmanager.bat' --licenses
 if ($env:INSTALL_LATEST_ONLY) {
     & '.\tools\bin\sdkmanager.bat' --sdk_root=$sdk_root `
         "platform-tools" `
@@ -74,6 +75,8 @@ else {
         "platforms;android-24" `
         "platforms;android-23" `
         "platforms;android-22" `
+        "platforms;android-21" `
+        "platforms;android-19" `
         "build-tools;30.0.2" `
         "build-tools;29.0.2" `
         "build-tools;29.0.0" `
@@ -100,6 +103,10 @@ else {
         "build-tools;23.0.3" `
         "build-tools;23.0.2" `
         "build-tools;23.0.1" `
+        "build-tools;22.0.1" `
+        "build-tools;21.1.2" `
+        "build-tools;20.0.0" `
+        "build-tools;19.1.0" `
         "extras;android;m2repository" `
         "extras;google;m2repository" `
         "extras;google;google_play_services" `
@@ -124,64 +131,7 @@ Remove-Item $sdkPath -Recurse -Force -ErrorAction Ignore
 $ErrorActionPreference = 'Stop'
 
 
-# else {
-#     & '.\tools\bin\sdkmanager.bat' --sdk_root=$sdk_root `
-#         "platform-tools" `
-#         "platforms;android-30" `
-#         "platforms;android-29" `
-#         "platforms;android-28" `
-#         "platforms;android-27" `
-#         "platforms;android-26" `
-#         "platforms;android-25" `
-#         "platforms;android-24" `
-#         "platforms;android-23" `
-#         "platforms;android-22" `
-#         "platforms;android-21" `
-#         "platforms;android-19" `
-#         "build-tools;30.0.2" `
-#         "build-tools;29.0.2" `
-#         "build-tools;29.0.0" `
-#         "build-tools;28.0.3" `
-#         "build-tools;28.0.2" `
-#         "build-tools;28.0.1" `
-#         "build-tools;28.0.0" `
-#         "build-tools;27.0.3" `
-#         "build-tools;27.0.2" `
-#         "build-tools;27.0.1" `
-#         "build-tools;27.0.0" `
-#         "build-tools;26.0.3" `
-#         "build-tools;26.0.2" `
-#         "build-tools;26.0.1" `
-#         "build-tools;26.0.0" `
-#         "build-tools;25.0.3" `
-#         "build-tools;25.0.2" `
-#         "build-tools;25.0.1" `
-#         "build-tools;25.0.0" `
-#         "build-tools;24.0.3" `
-#         "build-tools;24.0.2" `
-#         "build-tools;24.0.1" `
-#         "build-tools;24.0.0" `
-#         "build-tools;23.0.3" `
-#         "build-tools;23.0.2" `
-#         "build-tools;23.0.1" `
-#         "build-tools;22.0.1" `
-#         "build-tools;21.1.2" `
-#         "build-tools;20.0.0" `
-#         "build-tools;19.1.0" `
-#         "extras;android;m2repository" `
-#         "extras;google;m2repository" `
-#         "extras;google;google_play_services" `
-#         "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2" `
-#         "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.1" `
-#         "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2" `
-#         "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.1" `
-#         "add-ons;addon-google_apis-google-24" `
-#         "add-ons;addon-google_apis-google-23" `
-#         "add-ons;addon-google_apis-google-22" `
-#         "add-ons;addon-google_apis-google-21" `
-#         "cmake;3.6.4111459" `
-#         "patcher;v4" | Out-File -Width 240 -FilePath "$env:TEMP\android-sdkmanager.log"
-# }
+
 
 
 
