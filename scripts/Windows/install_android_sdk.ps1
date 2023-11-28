049c9815f2baa45621e50581970ce3243ba0b388
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-11-27 22:28:47 -0800

    remove android sdk patcher

--- scripts/Windows/install_android_sdk.ps1
+++ scripts/Windows/install_android_sdk.ps1
@@ -119,8 +119,7 @@ else {
         "add-ons;addon-google_apis-google-23" `
         "add-ons;addon-google_apis-google-22" `
         "add-ons;addon-google_apis-google-21" `
-        "cmake;3.6.4111459" `
-        "patcher;v4" | Out-File -Width 240 -FilePath "$env:TEMP\android-sdkmanager.log"
+        "cmake;3.6.4111459" | Out-File -Width 240 -FilePath "$env:TEMP\android-sdkmanager.log"
 }
 
 7z a "$env:TEMP\android-sdkmanager.log.zip" "$env:TEMP\android-sdkmanager.log"
