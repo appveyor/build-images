5ea92472144b882e6d1c3ed824d404a13c51a02d
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-11-28 17:55:45 -0800

    direct call

--- scripts/Windows/install_android_sdk.ps1
+++ scripts/Windows/install_android_sdk.ps1
@@ -67,7 +67,7 @@ if ($env:INSTALL_LATEST_ONLY) {
         "patcher;v4" | Out-File -Width 240 -FilePath "$env:TEMP\android-sdkmanager.log"
 }
 else {
-    & '.\cmdline-tools\bin\sdkmanager.bat' --sdk_root=$sdk_root --install `
+    '.\cmdline-tools\bin\sdkmanager.bat' --sdk_root=$sdk_root --install `
         "platform-tools" `
         "platforms;android-30" `
         "platforms;android-29" `
