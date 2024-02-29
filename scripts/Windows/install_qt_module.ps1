18982708edb327e467760d2d54fa6ab8044bd235
OwenMcDonnell <omcdonnell@gmail.com>
2024-02-29 06:29:44 -0800

    try berkley mirror

--- scripts/Windows/install_qt_module.ps1
+++ scripts/Windows/install_qt_module.ps1
@@ -6,8 +6,9 @@ $QT_INSTALL_DIR = "C:\Qt"
 $QT_ROOT_URL = 'http://qt.mirror.constant.com/online/qtsdkrepository/windows_x86/desktop'
 
 if ($isLinux) {
+    $QT_ROOT_URL = 'https://mirrors.ocf.berkeley.edu/qt/online/qtsdkrepository/linux_x64/desktop/'
     #$QT_ROOT_URL = 'http://qt.mirror.constant.com/online/qtsdkrepository/linux_x64/desktop'
-    $QT_ROOT_URL = 'https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/'
+    #$QT_ROOT_URL = 'https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/'
 } elseif ($isMacOS) {
     $QT_ROOT_URL = 'http://qt.mirror.constant.com/online/qtsdkrepository/mac_x64/desktop'
 }
