06494b853f1bd7d1a813ffc919d026a50c1718ad
OwenMcDonnell <omcdonnell@gmail.com>
2024-02-29 22:16:06 -0800

    bionic configure_mono override

--- scripts/Ubuntu/bionic.sh
+++ scripts/Ubuntu/bionic.sh
@@ -167,3 +167,13 @@ EOF
     log_version dpkg -l rabbitmq-server
     log_version erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell
 }
+
+function configure_mono_repository () {
+    echo "[INFO] Running configure_mono_repository on Ubuntu 20.04..."
+    
+    sudo apt-get install ca-certificates gnupg
+    sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    sudo apt-get update
+
+}
