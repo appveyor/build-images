2eeafcad08c7bbea79581663adf9bbce376d49dd
OwenMcDonnell <omcdonnell@gmail.com>
2024-03-02 11:56:34 -0800

    fix braces

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -869,7 +869,7 @@ function install_python_312(){
         minor_version="${python_version#*.}"
         minor_version="${minor_version%.*}"
         # Check if the current version is greater than or equal to the required version
-        if [[ $minor_version -lt $required_minor; then
+        if [ $minor_version -lt $required_minor ]; then
         # Exit with an error message
             echo "Error: Python version ${required_major}.${required_minor} or higher is required. You are using ${python_version}."
             exit 1
