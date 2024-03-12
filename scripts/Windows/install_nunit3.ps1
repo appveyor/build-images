bddee9db7d9eb0147ee30157d10b4ac26a536df9
OwenMcDonnell <omcdonnell@gmail.com>
2024-03-12 14:13:39 -0700

    NUnit 3.17.0

--- scripts/Windows/install_nunit3.ps1
+++ scripts/Windows/install_nunit3.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host "Installing NUnit 3.16.3..." -ForegroundColor Cyan -NoNewline
+﻿Write-Host "Installing NUnit 3.17.0..." -ForegroundColor Cyan -NoNewline
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -9,16 +9,15 @@ if (Test-Path $nunitPath) {
 }
 
 
-$zipPath = "$env:TEMP\NUnit.Console.3.16.3.nupkg"
+$zipPath = "$env:TEMP\NUnit.Console-3.17.0.zip"
 $tempPath = "$env:TEMP\NUnit.Console"
-    (New-Object Net.WebClient).DownloadFile('https://github.com/nunit/nunit-console/releases/download/3.16.3/NUnit.ConsoleRunner.3.16.3.nupkg', $zipPath)
+    (New-Object Net.WebClient).DownloadFile('https://github.com/nunit/nunit-console/releases/download/3.17.0/NUnit.Console-3.17.0.zip', $zipPath)
 7z x $zipPath -y -o"$tempPath" | Out-Null
 New-Item -Path "$nunitPath" -ItemType Directory -Force | Out-Null
-New-Item -Path "$nunitPath\bin" -ItemType Directory -Force | Out-Null
-Get-ChildItem -Path "$tempPath\tools\*.*" | Move-Item -Destination "$nunitPath\bin"
-#[IO.Directory]::Move("$tempPath\tools", "$nunitPath\bin")
-Copy-Item -Path "$tempPath\tools\agents" -Destination $nunitPath -Recurse
+[IO.Directory]::Move("$tempPath\bin\net35", "$nunitPath\bin")
+Copy-Item -Path "$tempPath\bin\agents" -Destination $nunitPath -Recurse
 Remove-Item $zipPath
+
 
 # logger
 $zipPath = "$($env:TEMP)\Appveyor.NUnit3Logger.zip"
