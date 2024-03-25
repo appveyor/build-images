4a848b48d0587651b2052847a494c716a218df07
OwenMcDonnell <omcdonnell@gmail.com>
2024-03-24 23:06:31 -0700

    fix boost 1.84.0 paths

--- scripts/Windows/install_boost_1.84.0_vs2019.ps1
+++ scripts/Windows/install_boost_1.84.0_vs2019.ps1
@@ -12,28 +12,28 @@ Measure-Command {
     Write-Host "Installing boost 1.84.0..." -ForegroundColor Cyan
 
     Write-Host "Downloading x86..."
-    $exePath = "$env:TEMP\boost_1_840-msvc-14.2-32.exe"
-    (New-Object Net.WebClient).DownloadFile('https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_840-msvc-14.2-32.exe', $exePath)
+    $exePath = "$env:TEMP\boost_1_84_0-msvc-14.2-32.exe"
+    (New-Object Net.WebClient).DownloadFile('https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_84_0-msvc-14.2-32.exe', $exePath)
 
     Write-Host "Installing x86..."
     cmd /c start /wait "$exePath" /verysilent
     del $exePath
     
     Write-Host "Downloading x64..."
-    $exePath = "$env:TEMP\boost_1_840-msvc-14.2-64.exe"
-    (New-Object Net.WebClient).DownloadFile('https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_840-msvc-14.2-64.exe', $exePath)
+    $exePath = "$env:TEMP\boost_1_84_0-msvc-14.2-64.exe"
+    (New-Object Net.WebClient).DownloadFile('https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_84_0-msvc-14.2-64.exe', $exePath)
 
     Write-Host "Installing x64..."
     cmd /c start /wait "$exePath" /verysilent
     del $exePath
 
-    [IO.Directory]::Move('C:\local\boost_1_840', 'C:\Libraries\boost_1_840')
+    [IO.Directory]::Move('C:\local\boost_1_84_0', 'C:\Libraries\boost_1_84_0')
 
     Remove-Item 'C:\local' -Force -Recurse
 
     Write-Host "Compressing..."
 
-    Start-ProcessWithOutput "compact /c /i /q /s:C:\Libraries\boost_1_840" -IgnoreStdOut
+    Start-ProcessWithOutput "compact /c /i /q /s:C:\Libraries\boost_1_84_0" -IgnoreStdOut
 }
 
 $StopWatch.Stop()
