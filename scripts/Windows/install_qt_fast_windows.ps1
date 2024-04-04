4d290cc060823d8b45f8e05bc4f6ea9428e6e0b2
OwenMcDonnell <omcdonnell@gmail.com>
2024-04-03 20:46:55 -0700

    Cmake 3.29.1, Qt 6.7.0

--- scripts/Windows/install_qt_fast_windows.ps1
+++ scripts/Windows/install_qt_fast_windows.ps1
@@ -6,7 +6,7 @@ $installDir = "C:\Qt"
 
 $component_groups = @(
     @{
-        version    = "6.6.2"
+        version    = "6.7.0"
         components = @(
             "win64_mingw",
             "win64_msvc2019_64",
@@ -523,49 +523,49 @@ if (-not $env:INSTALL_LATEST_ONLY) {
                 "qtquicktimeline.win64_msvc2019_64"
             )
         }        
-        @{
-            version    = "5.9.9"
-            components = @(
-                "win32_mingw53",
-                "win32_msvc2015",
-                "win64_msvc2017_64",
-                "qtcharts",
-                "qtcharts.win32_mingw53",
-                "qtcharts.win32_msvc2015",
-                "qtcharts.win64_msvc2017_64",
-                "qtdatavis3d",
-                "qtdatavis3d.win32_mingw53",
-                "qtdatavis3d.win32_msvc2015",
-                "qtdatavis3d.win64_msvc2017_64",
-                "qtnetworkauth",
-                "qtnetworkauth.win32_mingw53",
-                "qtnetworkauth.win32_msvc2015",
-                "qtnetworkauth.win64_msvc2017_64",
-                "qtpurchasing",
-                "qtpurchasing.win32_mingw53",
-                "qtpurchasing.win32_msvc2015",
-                "qtpurchasing.win64_msvc2017_64",
-                "qtremoteobjects",
-                "qtremoteobjects.win32_mingw53",
-                "qtremoteobjects.win32_msvc2015",
-                "qtremoteobjects.win64_msvc2017_64",
-                "qtscript",
-                "qtscript.win32_mingw53",
-                "qtscript.win32_msvc2015",
-                "qtscript.win64_msvc2017_64",
-                "qtspeech",
-                "qtspeech.win32_mingw53",
-                "qtspeech.win32_msvc2015",
-                "qtspeech.win64_msvc2017_64",
-                "qtvirtualkeyboard",
-                "qtvirtualkeyboard.win32_mingw53",
-                "qtvirtualkeyboard.win32_msvc2015",
-                "qtvirtualkeyboard.win64_msvc2017_64",
-                "qtwebengine",
-                "qtwebengine.win32_msvc2015",
-                "qtwebengine.win64_msvc2017_64"   
-            )
-        }
+        # @{
+        #     version    = "5.9.9"
+        #     components = @(
+        #         "win32_mingw53",
+        #         "win32_msvc2015",
+        #         "win64_msvc2017_64",
+        #         "qtcharts",
+        #         "qtcharts.win32_mingw53",
+        #         "qtcharts.win32_msvc2015",
+        #         "qtcharts.win64_msvc2017_64",
+        #         "qtdatavis3d",
+        #         "qtdatavis3d.win32_mingw53",
+        #         "qtdatavis3d.win32_msvc2015",
+        #         "qtdatavis3d.win64_msvc2017_64",
+        #         "qtnetworkauth",
+        #         "qtnetworkauth.win32_mingw53",
+        #         "qtnetworkauth.win32_msvc2015",
+        #         "qtnetworkauth.win64_msvc2017_64",
+        #         "qtpurchasing",
+        #         "qtpurchasing.win32_mingw53",
+        #         "qtpurchasing.win32_msvc2015",
+        #         "qtpurchasing.win64_msvc2017_64",
+        #         "qtremoteobjects",
+        #         "qtremoteobjects.win32_mingw53",
+        #         "qtremoteobjects.win32_msvc2015",
+        #         "qtremoteobjects.win64_msvc2017_64",
+        #         "qtscript",
+        #         "qtscript.win32_mingw53",
+        #         "qtscript.win32_msvc2015",
+        #         "qtscript.win64_msvc2017_64",
+        #         "qtspeech",
+        #         "qtspeech.win32_mingw53",
+        #         "qtspeech.win32_msvc2015",
+        #         "qtspeech.win64_msvc2017_64",
+        #         "qtvirtualkeyboard",
+        #         "qtvirtualkeyboard.win32_mingw53",
+        #         "qtvirtualkeyboard.win32_msvc2015",
+        #         "qtvirtualkeyboard.win64_msvc2017_64",
+        #         "qtwebengine",
+        #         "qtwebengine.win32_msvc2015",
+        #         "qtwebengine.win64_msvc2017_64"   
+        #     )
+        # }
     )
 }
 
@@ -605,11 +605,11 @@ Write-Host "OK" -ForegroundColor Green
 # set aliases
 $sym_links = @{
     "latest" = "5.15.2"
-    "6.6"    = "6.6.2"
+    "6.7"    = "6.7.0"
     "6.5"    = "6.5.3"
     "6.2"    = "6.2.4"
     "5.15"   = "5.15.2"
-    "5.9"    = "5.9.9"
+    #"5.9"    = "5.9.9"
 }
 
 foreach ($link in $sym_links.Keys) {
