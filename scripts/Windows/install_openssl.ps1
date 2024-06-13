8fd357152989fa59ee367522eab1031a725a2637
owen mcdonnell <omcdonnell@gmail.com>
2024-06-13 07:30:55 -0700

    OpenSSL 3.3.1

--- scripts/Windows/install_openssl.ps1
+++ scripts/Windows/install_openssl.ps1
@@ -136,10 +136,10 @@ UninstallOpenSSL "OpenSSL 3.2.2 (32-bit)"
 UninstallOpenSSL "OpenSSL 3.2.2 (64-bit)"
 
 
-Write-Host "Installing OpenSSL 3.3.0 32-bit ..." -ForegroundColor Cyan
+Write-Host "Installing OpenSSL 3.3.1 32-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win32OpenSSL-3_3_0.exe"
-(New-Object Net.WebClient).DownloadFile("https://slproweb.com/download/Win32OpenSSL-3_3_0.exe", $exePath)
+$exePath = "$env:temp\Win32OpenSSL-3_3_1.exe"
+(New-Object Net.WebClient).DownloadFile("https://slproweb.com/download/Win32OpenSSL-3_3_1.exe", $exePath)
 if (-not (Test-Path $exePath)) { throw "Unable to find $exePath" }
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v33-Win32-temp
@@ -147,10 +147,10 @@ Remove-Item $exePath
 Write-Host "Installed" -ForegroundColor Green
 Copy-Item C:\OpenSSL-v33-Win32-temp -Destination C:\OpenSSL-v33-Win32 -Recurse
 
-Write-Host "Installing OpenSSL 3.3.0 64-bit ..." -ForegroundColor Cyan
+Write-Host "Installing OpenSSL 3.3.1 64-bit ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
-$exePath = "$env:temp\Win64OpenSSL-3_3_0.exe"
-(New-Object Net.WebClient).DownloadFile("https://slproweb.com/download/Win64OpenSSL-3_3_0.exe", $exePath)
+$exePath = "$env:temp\Win64OpenSSL-3_3_1.exe"
+(New-Object Net.WebClient).DownloadFile("https://slproweb.com/download/Win64OpenSSL-3_3_1.exe", $exePath)
 if (-not (Test-Path $exePath)) { throw "Unable to find $exePath" }
 Write-Host "Installing..."
 cmd /c start /wait $exePath /silent /verysilent /sp- /suppressmsgboxes /DIR=C:\OpenSSL-v33-Win64-temp
@@ -158,8 +158,8 @@ Remove-Item $exePath
 Write-Host "Installed" -ForegroundColor Green
 Copy-Item C:\OpenSSL-v33-Win64-temp -Destination C:\OpenSSL-v33-Win64 -Recurse
 
-UninstallOpenSSL "OpenSSL 3.3.0 (32-bit)"
-UninstallOpenSSL "OpenSSL 3.3.0 (64-bit)"
+UninstallOpenSSL "OpenSSL 3.3.1 (32-bit)"
+UninstallOpenSSL "OpenSSL 3.3.1 (64-bit)"
 # -----------------------------------------------------------------------------------------------------------------
 
 Write-Host "Installing OpenSSL 1.0.2$102Letter 32-bit ..." -ForegroundColor Cyan
