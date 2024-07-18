a9c334315d98b78781eef10fe3ab5a18b18a82bc
owen mcdonnell <omcdonnell@gmail.com>
2024-07-17 21:47:58 -0700

    virtualbox 7.0.18

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -2142,7 +2142,7 @@ function install_virtualbox_core() {
 function install_virtualbox() {
     echo "[INFO] Running install_virtualbox..."
 
-    local VERSION=6.1.28
+    local VERSION=7.0.18
     local VBE_URL=https://download.virtualbox.org/virtualbox/${VERSION}/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
 
     install_virtualbox_core || return $?
