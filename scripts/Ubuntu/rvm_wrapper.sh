c40660e7250666c7a9a0954d44c89d7c3b3aaaf7
owen mcdonnell <omcdonnell@gmail.com>
2024-07-22 07:43:25 -0700

    update ruby versions in shim

--- scripts/Ubuntu/rvm_wrapper.sh
+++ scripts/Ubuntu/rvm_wrapper.sh
@@ -14,16 +14,17 @@ fi
 
 declare -A RubyVersions
 
-RubyVersions[2.1]=2.1.10
-RubyVersions[2.2]=2.2.10
-RubyVersions[2.3]=2.3.8
+# RubyVersions[2.1]=2.1.10
+# RubyVersions[2.2]=2.2.10
+# RubyVersions[2.3]=2.3.8
 RubyVersions[2.4]=2.4.10
 RubyVersions[2.5]=2.5.9
 RubyVersions[2.6]=2.6.10
 RubyVersions[2.7]=2.7.8
 RubyVersions[3.0]=3.0.6
-RubyVersions[3.1]=3.1.4
-RubyVersions[3.2]=3.2.3
+RubyVersions[3.1]=3.1.5
+RubyVersions[3.2]=3.2.4
+RubyVersions[3.3]=3.3.4
 
 if [[ -v RubyVersions[$VERSION] ]]; then
   echo "Ruby version found: ${RubyVersions[$VERSION]}"
