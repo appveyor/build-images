15d627dc4e192a297339908a45c6ff116bbcf180
owen mcdonnell <omcdonnell@gmail.com>
2024-07-30 16:13:09 -0700

    re-run 22.04 with new base image

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -419,7 +419,7 @@ function install_azure_linux_agent(){
 
 function copy_appveyoragent() {
     if [[ -z "${APPVEYOR_BUILD_AGENT_VERSION-}" || "${#APPVEYOR_BUILD_AGENT_VERSION}" = "0" ]]; then
-        APPVEYOR_BUILD_AGENT_VERSION=7.0.3279;
+        APPVEYOR_BUILD_AGENT_VERSION=7.0.3311;
     fi
 
     echo "[INFO] Installing AppVeyor Build Agent v${APPVEYOR_BUILD_AGENT_VERSION}"
--- scripts/Ubuntu/jammy.sh
+++ scripts/Ubuntu/jammy.sh
@@ -133,8 +133,11 @@ function install_gcc() {
     update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 40 --slave /usr/bin/g++ g++ /usr/bin/g++-11 ||
         { echo "[ERROR] Cannot install gcc-11." 1>&2; return 40; }
     apt-get -y -q install gcc-12 g++-12 && \
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 40 --slave /usr/bin/g++ g++ /usr/bin/g++-12 ||
-        { echo "[ERROR] Cannot install gcc-12." 1>&2; return 40; }
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 50 --slave /usr/bin/g++ g++ /usr/bin/g++-12 ||
+        { echo "[ERROR] Cannot install gcc-12." 1>&2; return 50; }
+    apt-get -y -q install gcc-13 g++-13 && \
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 60 --slave /usr/bin/g++ g++ /usr/bin/g++-13 ||
+        { echo "[ERROR] Cannot install gcc-13." 1>&2; return 60; }
 }
 
 function install_clang() {
