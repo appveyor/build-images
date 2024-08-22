cb71543fa52bc468e7dd8411e8df54e8f94c26e6
owen mcdonnell <omcdonnell@gmail.com>
2024-08-21 20:36:29 -0700

    LLVM 18.1.8

--- scripts/Windows/install_llvm.ps1
+++ scripts/Windows/install_llvm.ps1
@@ -1,4 +1,4 @@
-$llvmVersion = "18.1.6"
+$llvmVersion = "18.1.8"
 Write-Host "Installing LLVM $llvmVersion ..." -ForegroundColor Cyan
 Write-Host "Downloading..."
 $exePath = "$env:temp\LLVM-$llvmVersion-win64.exe"
