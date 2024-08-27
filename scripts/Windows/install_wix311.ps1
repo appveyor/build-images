34bc845627734b6f02def647c26c37bb27055db2
owen mcdonnell <omcdonnell@gmail.com>
2024-08-27 15:17:42 -0700

    wix 3.14.1

--- scripts/Windows/install_wix311.ps1
+++ scripts/Windows/install_wix311.ps1
@@ -1,8 +1,8 @@
-﻿Write-Host "Installing WiX 3.11.1 ..." -ForegroundColor Cyan
+﻿Write-Host "Installing WiX 3.14.1 ..." -ForegroundColor Cyan
 
 Write-Host "Downloading..."
 $exePath = "$env:TEMP\wix311.exe"
-(New-Object Net.WebClient).DownloadFile('https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/wixtoolset/wix3/releases/download/wix3141rtm/wix314.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait "$exePath" /q
