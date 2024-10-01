603e4072905889b87b9bf9dfb444e3015871c731
Feodor Fitsner <feodor@appveyor.com>
2024-10-01 09:29:29 -0700

    Python 3.12.6

--- scripts/Ubuntu/common.sh
+++ scripts/Ubuntu/common.sh
@@ -789,9 +789,9 @@ function install_pip3() {
 function install_pythons(){
     echo "[INFO] Running install_pythons..."
     if [[ $OS_ARCH == "amd64" ]]; then
-        declare PY_VERSIONS=( "2.7.18" "3.6.15" "3.7.16" "3.8.17" "3.9.18" "3.10.14" "3.11.9" "3.13.0b4" "3.12.4" )
+        declare PY_VERSIONS=( "2.7.18" "3.6.15" "3.7.16" "3.8.17" "3.9.18" "3.10.14" "3.11.9" "3.13.0b4" "3.12.6" )
     else
-        declare PY_VERSIONS=( "2.7.18" "3.7.16" "3.8.17" "3.9.17" "3.10.12" "3.11.4" "3.12.0" )
+        declare PY_VERSIONS=( "2.7.18" "3.7.16" "3.8.17" "3.9.17" "3.10.12" "3.11.4" "3.12.6" )
     fi
 
     for i in "${PY_VERSIONS[@]}"; do
