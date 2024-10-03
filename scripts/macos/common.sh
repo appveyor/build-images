7f1a7dfd38c1d82108177e13aa759d6b9d248add
Feodor Fitsner <feodor@appveyor.com>
2024-10-03 16:27:21 -0700

    Remove homebrew/cask-versions

--- scripts/macos/common.sh
+++ scripts/macos/common.sh
@@ -779,11 +779,7 @@ function install_openjdk() {
         # # big sur, monterey
         # if [ "$OSX_MAJOR_VER" -ge 11 ]; then
         #     JDK_VERSIONS=( "15" "16" "17" "18" "19" )
-        # fi
-
-        su -l ${USER_NAME} -c "
-            $BREW_CMD tap homebrew/cask-versions
-        " || { echo "[ERROR] Cannot add AdoptOpenJDK/openjdk tap." 1>&2; return 20; }        
+        # fi 
 
         # install JDKs
         for JDK_VERSION in "${JDK_VERSIONS[@]}"; do
