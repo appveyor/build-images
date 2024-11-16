961edbcb7a303220292fc98b392279dee9589861
Owen McDonnell <owen@appveyor.com>
2024-11-16 13:51:38 -0800

    fix get-version

--- scripts/Windows/install_nodejs_latest.ps1
+++ scripts/Windows/install_nodejs_latest.ps1
@@ -223,7 +223,7 @@ function Install-Nodejs {
 
     for ($i = 0; $i -lt $nodeVersions.Length; $i++) {
         for ($j = 0; $j -lt $nodePlatforms.Length; $j++) {
-            $v = Get-Version $version
+            $v = Get-Version $nodeVersions[$i]
             Write-Host "Node major version: ${$v.Major}"
             $nodeVersion = $nodeVersions[$i]
             $nodePlatform = $nodePlatforms[$j]
