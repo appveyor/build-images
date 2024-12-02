1309b2e40b6b4349f4ad65d51548089629ec2d01
Owen McDonnell <owen@appveyor.com>
2024-12-02 10:09:20 -0800

    try overwrite mode

--- scripts/Windows/install_octo.ps1
+++ scripts/Windows/install_octo.ps1
@@ -13,7 +13,7 @@ $zipPath = "$env:TEMP\OctoCli.zip"
 (New-Object Net.WebClient).DownloadFile('https://github.com/OctopusDeploy/cli/releases/download/v2.11.0/octopus_2.11.0_windows_amd64.zip', $zipPath)
 (New-Object Net.WebClient).DownloadFile('https://download.octopusdeploy.com/octopus-tools/6.17.0/OctopusTools.6.17.0.zip', $oldZipPath)
 7z x $oldZipPath -aoa -o"$destPath"
-7z x $zipPath -o"$destPath"
+7z x $zipPath -aoa -o"$destPath"
 gci $destPath -r | select -exp FullName
 Remove-Item $zipPath
 
