93887782599f80c2372e0d3033d923b9f276a93d
Owen McDonnell <owen@appveyor.com>
2024-12-02 09:55:50 -0800

    fix octo

--- scripts/Windows/install_octo.ps1
+++ scripts/Windows/install_octo.ps1
@@ -8,15 +8,19 @@ if(-not (Test-Path $destPath)) {
     New-Item $destPath -ItemType directory -Force | Out-Null
 }
 
-$zipPath = "$env:TEMP\OctopusTools.zip"
+$oldZipPath = "$env:TEMP\OctopusTools.zip"
+$zipPath = "$env:TEMP\OctoCli.zip"
 (New-Object Net.WebClient).DownloadFile('https://github.com/OctopusDeploy/cli/releases/download/v2.11.0/octopus_2.11.0_windows_amd64.zip', $zipPath)
-7z x $zipPath -aoa -o"$destPath"
+(New-Object Net.WebClient).DownloadFile('https://download.octopusdeploy.com/octopus-tools/6.17.0/OctopusTools.6.17.0.zip', $oldZipPath)
+7z x $oldZipPath -aoa -o"$destPath"
+7z x $zipPath -o"$destPath"
+gci $destPath -r | select -exp FullName
 Remove-Item $zipPath
 
 Add-Path $destPath
 Add-SessionPath $destPath
 
-#cmd /c octo --version
+cmd /c octo --version
 cmd /c octopus --version
 
 Write-Host "Installed Octopus tools" -ForegroundColor Green
