d50e1c1575e0371c410089af60023be4f79cbb47
Owen McDonnell <omcdonnell@gmail.com>
2025-08-07 21:41:04 -0700

    update bionic chrome, update rvm_wrapper.sh versions

--- scripts/Ubuntu/bionic.sh
+++ scripts/Ubuntu/bionic.sh
@@ -85,7 +85,7 @@ function install_doxygen() {
 
 function install_google_chrome() {
     echo "[INFO] Running install_google_chrome on Bionic..."
-    local CHROME_VERSION=122.0.6261.128-1
+    local CHROME_VERSION=138.0.7204.183-1
     local DEBNAME=google-chrome-stable_${CHROME_VERSION}_amd64.deb
     curl -fsSL -O https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/${DEBNAME}
     sudo dpkg -i ${DEBNAME}
