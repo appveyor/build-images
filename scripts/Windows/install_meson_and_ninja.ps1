7a88238cade33c58b54a0f35baf02ab756332aba
Owen McDonnell <omcdonnell@gmail.com>
2025-08-24 12:31:45 -0700

    Meson 1.8.0, Ninja 1.13.1

--- scripts/Windows/install_meson_and_ninja.ps1
+++ scripts/Windows/install_meson_and_ninja.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Host "Installing Meson and Ninja..." -ForegroundColor Cyan
 
-$mesonUrl = 'https://github.com/mesonbuild/meson/releases/download/1.3.0/meson-1.3.0-64.msi'
-$ninjaUrl = 'https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-win.zip'
+$mesonUrl = 'https://github.com/mesonbuild/meson/releases/download/1.8.0/meson-1.8.0-64.msi'
+$ninjaUrl = 'https://github.com/ninja-build/ninja/releases/download/v1.13.1/ninja-win.zip'
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\meson.msi"
