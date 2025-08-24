1276b7ab8ab42d3266f11bfd93d9d97222c5c774
Owen McDonnell <omcdonnell@gmail.com>
2025-08-24 12:35:59 -0700

    Perl 5.40.2.1

--- scripts/Windows/install_perl.ps1
+++ scripts/Windows/install_perl.ps1
@@ -1,9 +1,9 @@
-Write-Host "Installing Strawberry Perl 5.32.1.1..." -ForegroundColor Cyan
+Write-Host "Installing Strawberry Perl 5.40.2.1..." -ForegroundColor Cyan
 
-$msiPath = "$env:TEMP\strawberry-perl-5.32.1.1-64bit.msi"
+$msiPath = "$env:TEMP\strawberry-perl-5.40.2.1-64bit.msi"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-64bit.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54021_64bit_UCRT/strawberry-perl-5.40.2.1-64bit.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /qn /norestart
