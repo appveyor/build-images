f2f0f8d253f2e92fc7cc858659d83e665ff3336f
Owen McDonnell <omcdonnell@gmail.com>
2025-11-06 08:25:11 -0800

    update/fix coverity

--- scripts/Windows/install_coverity.ps1
+++ scripts/Windows/install_coverity.ps1
@@ -6,7 +6,7 @@ if (Test-Path $destPath ) {
 }
 
 Write-Host "Downloading..."
-$zipPath = "$env:TEMP\cov-analysis-win64-2023.12.2.zip"
+$zipPath = "$env:TEMP\cov-analysis-win64-2024.12.1.zip"
 #(New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/cov-analysis-win64-2023.12.2.zip', $zipPath)
 (New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/cov-analysis-win64-2024.12.1.zip', $zipPath)
 
@@ -14,7 +14,7 @@ Write-Host "Unpacking..."
 $tempPath = "$env:TEMP\Coverity"
 7z x $zipPath -o"$tempPath" | Out-Null
 
-[IO.Directory]::Move("$tempPath\cov-analysis-win64-2023.12.2", $destPath)
+[IO.Directory]::Move("$tempPath\cov-analysis-win64-2024.12.1", $destPath)
 del $tempPath -Recurse -Force
 del $zipPath
 
