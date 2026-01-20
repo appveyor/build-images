6fe8a1fe6c28741f46aea03a5a3d745691229c24
Owen McDonnell <omcdonnell@gmail.com>
2026-01-20 15:09:10 -0800

    update nsis link

--- scripts/Windows/install_nsis.ps1
+++ scripts/Windows/install_nsis.ps1
@@ -1,8 +1,8 @@
-﻿Write-Host "Installing NSIS 3.10 ..." -ForegroundColor Cyan
-$exePath = "$($env:TEMP)\nsis-3.10-setup.exe"
+﻿Write-Host "Installing NSIS 3.11 ..." -ForegroundColor Cyan
+$exePath = "$($env:TEMP)\nsis-3.11-setup.exe"
 
 Write-Host "Downloading..."
-(New-Object Net.WebClient).DownloadFile('https://appveyordownloads.blob.core.windows.net/misc/nsis-3.10-setup.exe', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://sourceforge.net/projects/nsis/files/NSIS%203/3.11/nsis-3.11-setup.exe', $exePath)
 
 Write-Host "Installing..."
 cmd /c start /wait $exePath /S
