e7ad2d82dc480aaa1ac4b4bb276a35318727eae2
Owen McDonnell <omcdonnell@gmail.com>
2026-01-20 14:08:28 -0800

    fix service fabric links

--- scripts/Windows/install_service_fabric_new.ps1
+++ scripts/Windows/install_service_fabric_new.ps1
@@ -4,7 +4,7 @@ Write-Host "Installing Service Fabric 11.2.274.1" -ForegroundColor Cyan
 # install runtime
 Write-Host "Downloading Service Fabric Runtime..."
 $exePath = "$env:TEMP\MicrosoftServiceFabricRuntime.exe"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabric.10.0.2899.9590.exe', $exePath)
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabric.10.1.2941.9590.exe', $exePath)
 
 Write-Host "Installing Service Fabric Runtime..."
 cmd /c start /wait $exePath /AcceptEULA
@@ -14,7 +14,7 @@ Write-Host "Installing Service Fabric SDK..."
 
 Write-Host "Downloading..."
 $msiPath = "$env:TEMP\MicrosoftServiceFabricSDK.msi"
-(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.8.1.274.msi', $msiPath)
+(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/b/8/a/b8a2fb98-0ec1-41e5-be98-9d8b5abf7856/MicrosoftServiceFabricSDK.7.1.2941.msi', $msiPath)
 
 Write-Host "Installing..."
 cmd /c start /wait msiexec /i "$msiPath" /quiet
